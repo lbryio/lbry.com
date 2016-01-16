e34933262464f2cefbb91732bbb6929b5c5db04e
James Kiselak <jameskiselak@gmail.com>
2016-01-16 01:39:59 -0500

    change font for lbry_setup.sh

--- view/page/linux.php
+++ view/page/linux.php
@@ -15,7 +15,7 @@
             <code><span class="code-bash-kw2">mkdir</span> -p lbry</code>
             <code><span class="code-bash-kw1">cd</span> lbry</code>
             <code><span class="code-bash-kw2">wget</span> http:<span class="sy0">//</span>lbry.io<span class="sy0">/</span>dl<span class="sy0">/</span>lbry_setup.sh</code>
-            <code><span class="code-bash-kw2">bash lbry_setup.sh</span></code>
+            <code><span class="code-bash-kw2">bash</span> lbry_setup.sh</code>
             <code><span class="code-bash-kw1">lbrynet-console</span></code>
           </div>
         </li>
