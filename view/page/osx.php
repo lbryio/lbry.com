112d2a61e3c2bc3bb12933e0308a0b47488d58f2
Jeremy Kauffman <jeremy@usetopscore.com>
2016-03-10 20:29:09 -0500

    osx spacing/style

--- view/page/osx.php
+++ view/page/osx.php
@@ -2,8 +2,10 @@
 <?php ob_start() ?>
   <h1>Install LBRY on OS X <span class="icon-apple"></span> (command line)</h1>
   <?php echo View::render('get/alphaNotice') ?>
-  <a class="btn-primary" href="//lbry.io/osx.dmg">Download for OS X</a>
-  <p>Or, view the source and compile instructions on 
+  <p>
+    <a class="btn-primary" href="//lbry.io/osx.dmg">Download for OS X</a>
+  </p>
+  <p class="meta">Or, view the source and compile instructions on
     <a href="https://github.com/lbryio/lbry-setup/blob/master/README_OSX.md" class="link-primary">GitHub</a>.
   </p>
 <?php $html = ob_get_clean() ?>
