349596d4a96c39ed20b92736fb4bbfe7653413a7
Jack Robison <jack@robisonservice.com>
2016-03-26 17:58:45 -0400

    update os x link in get page

--- view/page/osx.php
+++ view/page/osx.php
@@ -3,7 +3,7 @@
   <h1>Install LBRY on OS X <span class="icon-apple"></span> (command line)</h1>
   <?php echo View::render('get/alphaNotice') ?>
   <p>
-    <a class="btn-primary" href="//lbry.io/osx.dmg">Download for OS X</a>
+    <a class="btn-primary" href="//lbry.io/lbry-osx-latest.dmg">Download for OS X</a>
   </p>
   <p class="meta">Or, view the source and compile instructions on
     <a href="https://github.com/lbryio/lbry-setup/blob/master/README_OSX.md" class="link-primary">GitHub</a>.
