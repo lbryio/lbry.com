3d852a666ee788167fd95a8b968903a1f6aed028
Alex Grintsvayg <grin@lbry.io>
2016-04-27 15:24:58 -0400

    update bio

--- view/page/team.php
+++ view/page/team.php
@@ -92,7 +92,7 @@
       </div>
     </div>
     <div class="row-fluid">
-   
+
       <div class="span6  spacer2">
         <div class="photo-container">
           <img src="/img/mike-644x450.jpg" alt="Mike Vine"/>
@@ -100,13 +100,13 @@
          <h4>Mike Vine <a href="mailto:mike@lbry.io" class="link-primary"><span class="icon icon-envelope"></span></a></h4>
           <div class="meta  spacer1">Founder, Evangelist</div>
           <p>
-            With a humble BA in Philosophy from <a href="http://tulane.edu/" class="link-primary">Tulane University</a>, Mike has 
-            built a successful financial services marketing company, <a href="http://www.centinel.net/" class="link-primary">Centinel Consulting</a>. 
+            With a humble BA in Philosophy from <a href="http://tulane.edu/" class="link-primary">Tulane University</a>, Mike has
+            built a successful financial services marketing company, <a href="http://www.centinel.net/" class="link-primary">Centinel Consulting</a>.
             Centinel has helped clients grow from close to nothing to hundreds of thousands of visitors. He manages
             email marketing lists and social media accounts of the same size.
           </p>
           <p>
-            Mike has been involved with the Bitcoin community since the early days. His friends have launched companies like 
+            Mike has been involved with the Bitcoin community since the early days. His friends have launched companies like
             <a class="link-primary" href="//lamassu.is">Lamassu BTM</a>,
             <a class="link-primary" href="//coinapult.com">Coinapult</a>,
             <a class="link-primary" href="//shapeshift.io">Shapeshift</a>.
@@ -125,10 +125,11 @@
             new technologies faster than they can be created.
           </p>
           <p>
-            Alex has designed and managed scalable infrastructure solutions for SaaS firms and is leveraging that
-            experience to ensure LBRY's infrastructure and architecture is scalable.
+            Alex designs and manages scalable infrastructure solutions for SaaS firms, and is
+            leveraging that experience to ensure LBRY's architecture is rock-solid.
           <p>
-            Alex attended RPI (receiving degrees in Computer Science and Psychology) and Stuyvesant High School.
+            Alex is the 3rd dual-degree graduate from RPI on this team, receiving degrees in
+            Computer Science and Psychology. He is also an alumni of Stuyvesant High School.
           </p>
       </div>
     </div>
@@ -148,10 +149,10 @@
             decide to revolutionize the internet.
           </p>
           <p>
-            Jack was one of the first people to discover LBRY and took to it so fast he may understand more 
+            Jack was one of the first people to discover LBRY and took to it so fast he may understand more
             about it than anyone.
           <p>
-            Jack has Asperger's Syndrome and is actively involved in the autism community. He was a regular on Wrong Planet's 
+            Jack has Asperger's Syndrome and is actively involved in the autism community. He was a regular on Wrong Planet's
             Autism Talk TV, has appeared on
             <em>National Public Radio</em>, the <em>New York Times</em>, and presents around the country.
           </p>
@@ -177,19 +178,19 @@
         </div>
         <h4>Alex Tabarrok</h4>
         <div class="meta  spacer1">Economic Advisor</div>
-        <p>Alex Tabarrok is Bartley J. Madden Chair in Economics at the <a href="http://mercatus.org/" class="link-primary">Mercatus Center</a> 
+        <p>Alex Tabarrok is Bartley J. Madden Chair in Economics at the <a href="http://mercatus.org/" class="link-primary">Mercatus Center</a>
           and a professor of economics at <a href="//gmu.edu" class="link-primary">George Mason University</a>. He specializes in intellectual property reform, the effectiveness of markets, and the justice system.
         </p>
-        <p>Tabarrok is the coauthor, with Mercatus colleague Tyler Cowen, of the popular economics blog <a class="link-primary" href="http://www.marginalrevolution.com/"><em>Marginal Revolution</em></a> 
-          and cofounder of the online educational platform <a class="link-primary" href="http://mruniversity.com/">Marginal Revolution University</a>. 
-          He is the coauthor of 
-          <em><a href="http://www.amazon.com/Modern-Principles-Economics-Tyler-Cowen/dp/1429239972" class="link-primary">Modern Principles of Economics</a></em>, 
+        <p>Tabarrok is the coauthor, with Mercatus colleague Tyler Cowen, of the popular economics blog <a class="link-primary" href="http://www.marginalrevolution.com/"><em>Marginal Revolution</em></a>
+          and cofounder of the online educational platform <a class="link-primary" href="http://mruniversity.com/">Marginal Revolution University</a>.
+          He is the coauthor of
+          <em><a href="http://www.amazon.com/Modern-Principles-Economics-Tyler-Cowen/dp/1429239972" class="link-primary">Modern Principles of Economics</a></em>,
           and author of the recent book
-          <em><a href="http://www.amazon.com/Launching-The-Innovation-Renaissance-Market-ebook/dp/B006C1HX24" class="link-primary">Launching the Innovation Renaissance</em></a>. 
-          His articles have appeared in the<em> New York Times</em>, the<em> Washington Post</em>, the<em> Wall Street Journal</em>, and many 
+          <em><a href="http://www.amazon.com/Launching-The-Innovation-Renaissance-Market-ebook/dp/B006C1HX24" class="link-primary">Launching the Innovation Renaissance</em></a>.
+          His articles have appeared in the<em> New York Times</em>, the<em> Washington Post</em>, the<em> Wall Street Journal</em>, and many
           other prestigious publications.
-        </p> 
-        <p>Tabarrok received his PhD in economics from 
+        </p>
+        <p>Tabarrok received his PhD in economics from
           <a class="link-primary" href="http://en.wikipedia.org/wiki/George_Mason_University" title="George Mason University">George Mason University</a>.
         </p>
       </div>
@@ -206,14 +207,14 @@
           <a href="http://www.amazon.com/International-Investment-Political-Dispute-Resolution/dp/0379215225" class="link-primary">
             <em>International Investment, Political Risk, and Dispute Resolution: A Practitioner’s Guide</em>
           </a>
-          and 
+          and
           <a href="https://mises.org/library/against-intellectual-property-0" class="link-primary">
             <em>Against Intellectual Property</em>
           </a>.
         </p>
-        <p> 
-          He received an LL.M. in international business law from <a href="http://www.kcl.ac.uk/" class="link-primary">King’s College London</a>, a JD from the Paul M. Hebert Law Center at 
-          <a href="//lsu.edu" class="link-primary">Lousiana State University</a>, 
+        <p>
+          He received an LL.M. in international business law from <a href="http://www.kcl.ac.uk/" class="link-primary">King’s College London</a>, a JD from the Paul M. Hebert Law Center at
+          <a href="//lsu.edu" class="link-primary">Lousiana State University</a>,
           as well as BSEE and MSEE degrees. His websites are <a href="stephankinsella.com" class="link-primary">stephankinsella.com</a>
           and <a href="kinsellalaw.com" class="link-primary">kinsellalaw.com</a>
         </p>
