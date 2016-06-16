dba4b5aa297129e766d4a4da5ee5fd69586e2eaa
Jeremy Kauffman <jeremy@usetopscore.com>
2016-06-16 16:20:33 -0400

    fix typo in reilly bio

--- view/template/page/team.php
+++ view/template/page/team.php
@@ -167,7 +167,7 @@
           <div class="meta  spacer1">Curator</div>
           <p>
           Reilly Smith serves as the in-house Curator and content liaison for LBRY. 
-          He has worked in the entertainment industry since 2010, having produced two indie two feature films and various short content. His films have shown at Sundance, SXSW, Dallas IFF, and LA Film Fest and he has produced series for Zero Day Fox and Disney.
+          He has worked in the entertainment industry since 2010, having produced two indie feature films and various short content. His films have shown at Sundance, SXSW, Dallas IFF, and LA Film Fest and he has produced series for Zero Day Fox and Disney.
           </p>
           <p>
           Reilly holds a BA in Film and Media Studies from the University of Oklahoma.
