7bc4a483c09cd10ce1bf7c97305f4f443eebbb99
rezzme <reillysmith@gmail.com>
2016-06-29 18:53:48 -0700

    Update home.php

--- view/template/page/home.php
+++ view/template/page/home.php
@@ -43,7 +43,7 @@
           </div>
         </div>
         <div class="video" style="margin-bottom: 80px">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/qkUA0vTWM7g" frameborder="0" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/HntqDpqmjUs" frameborder="0" allowfullscreen></iframe>
         </div>
       </div>
       <div class="content content-dark">
