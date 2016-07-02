c138a802027c7a98feed2914b33ba1731c7d5681
rezzme <reillysmith@gmail.com>
2016-07-02 11:54:00 -0700

    Update _videoIntro.php

--- view/template/download/_videoIntro.php
+++ view/template/download/_videoIntro.php
@@ -1,3 +1,3 @@
 <div class="video">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/HntqDpqmjUs" frameborder="0" allowfullscreen></iframe>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/DjouYBEkQPY" frameborder="0" allowfullscreen></iframe>
 </div>
