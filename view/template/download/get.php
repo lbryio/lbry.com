f8cb9459d51a9d39d322a567c2a61ad5f5e30d2b
Jeremy Kauffman <kauffj@gmail.com>
2016-08-06 10:23:52 -0400

    clarify you are on the list after providing your email

--- view/template/download/get.php
+++ view/template/download/get.php
@@ -12,6 +12,9 @@
       <?php endif ?>
 
       <?php if (Session::get(Session::KEY_PREFINERY_USER_ID)): ?>
+        <h3>You're On The List</h3>
+        <p>We pull thousands of people off the list regularly.</p>
+        <p>The longer you are on the list and the more people you invite, the sooner you will be invited.</p>
         <?php echo View::render('download/_refer') ?>
       <?php else: ?>
         <p>{{download.signup}}</p>
