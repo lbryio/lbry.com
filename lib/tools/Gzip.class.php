05b9b2820caafcf1c1da9a15c287c29d9c8354c4
Alex Grintsvayg <alex@usetopscore.com>
2016-09-02 11:35:22 -0400

    is this better compression?

---
+++ lib/tools/Gzip.class.php
@@ -0,0 +1,35 @@
+<?php
+
+class Gzip
+{
+  public static function compressFile($source, $level = 1)
+  {
+    $compressedPath  = $source . '.gz';
+    $mode  = 'wb' . $level;
+
+    $fpOut = gzopen($compressedPath, $mode);
+    if (!$fpOut)
+    {
+      return false;
+    }
+
+    $fpIn = fopen($source, 'rb');
+    $error = false;
+    if ($fpIn)
+    {
+      while (!feof($fpIn))
+      {
+        gzwrite($fpOut, fread($fpIn, 1024 * 512));
+      }
+      fclose($fpIn);
+    }
+    else
+    {
+      $error = true;
+    }
+
+    gzclose($fpOut);
+
+    return $error ? false : $compressedPath;
+  }
+}
