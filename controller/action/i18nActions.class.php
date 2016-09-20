e50e7290193a06b80a7bc6dd82bf8689e92583df
Alex Grintsvayg <grin@lbry.io>
2016-09-20 09:55:15 -0400

    minor

--- controller/action/i18nActions.class.php
+++ controller/action/i18nActions.class.php
@@ -1,36 +1,29 @@
 <?php
 
-/**
- * Description of i18nActions
- *
- * @author Nacib Neme
- */
-
 class i18nActions extends Actions
 {
-    public static function setCulture()
+  public static function setCulture()
+  {
+    $culture = Request::getPostParam('culture');
+
+    // Validate
+    if ($culture && !in_array($culture, i18n::getAllCultures()))
     {
-        $culture = $_POST['culture'] ?? null;
-
-        // Validate
-        if ($culture && !in_array($culture, i18n::getAllCultures()))
-        {
-            $culture = null;
-        }
-
-        if ($culture)
-        {
-            Session::set(Session::KEY_USER_CULTURE, $culture);
-        }
-
-        else
-        {
-            Session::unsetKey(Session::KEY_USER_CULTURE);
-        }
-
-        //if session changes update domain
-        //english language = www
-
-        return Controller::redirect($_SERVER['HTTP_REFERER'] ?: '/');
+      $culture = null;
     }
+
+    if ($culture)
+    {
+      Session::set(Session::KEY_USER_CULTURE, $culture);
+    }
+    else
+    {
+      Session::unsetKey(Session::KEY_USER_CULTURE);
+    }
+
+    //if session changes update domain
+    //english language = www
+
+    return Controller::redirect(Request::getHttpHeader('Referer', '/'));
+  }
 }
