83e5d1928ee65dabff7a0f1210125e4c6ba3ba77
Josh Finer <josh@lbry.io>
2016-11-11 14:08:51 -0500

    Update thanks.php
    Google conversion code

--- view/template/acquisition/thanks.php
+++ view/template/acquisition/thanks.php
@@ -4,4 +4,23 @@
     <h1>Thank You!</h1>
     <p>Your account will be synced. If you have any questions, please contact <a href="mailto:reilly@lbry.io" class="link-primary">reilly@lbry.io</a></p>
   </div>
+  <!-- Google Code for Lead Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 980489749;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "B0ZpCIuLgV0QlazE0wM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/980489749/?label=B0ZpCIuLgV0QlazE0wM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
 </main>
