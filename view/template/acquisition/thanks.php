89345a16f7438b709926f58df5c85ab2be79e8ac
Josh Finer <josh@lbry.io>
2016-11-11 14:29:23 -0500

    Update thanks.php
    fb pixel

--- view/template/acquisition/thanks.php
+++ view/template/acquisition/thanks.php
@@ -4,6 +4,21 @@
     <h1>Thank You!</h1>
     <p>Your account will be synced. If you have any questions, please contact <a href="mailto:reilly@lbry.io" class="link-primary">reilly@lbry.io</a></p>
   </div>
+  <!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1618717031725766');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1618717031725766&ev=PageView&noscript=1"
+/></noscript>
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
   <!-- Google Code for Lead Conversion Page -->
 <script type="text/javascript">
 /* <![CDATA[ */
