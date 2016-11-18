744c9848d17b09515d096105c1bb4e1bc8361bfc
Alex Grintsvayg <alex@grin.io>
2016-11-18 09:14:10 -0500

    better syntax check

--- check_syntax.sh
+++ check_syntax.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ERRORS=0
 
 for FILE in $(find "$DIR" -name '*.php'); do
-  php7.0 -l $FILE
+  CHECK=$(php7.0 -l $FILE | grep -v "^No syntax errors detected")
+  if [ -n "$CHECK" ]; then
+    echo "$CHECK"
+    ERRORS=1
+  fi
 done
+
+if [ $ERRORS -eq 0 ]; then
+  echo "Syntax OK"
+fi
+
+exit $ERRORS
