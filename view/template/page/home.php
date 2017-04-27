1d9c399cfcbe4e4ff93688cd7d3694199a4e7f3f
Jeremy Kauffman <kauffj@gmail.com>
2017-04-27 16:08:45 -0400

    ship it

--- view/template/page/home.php
+++ view/template/page/home.php
@@ -1,44 +1,24 @@
-<div class="bg-image-full" style="background-image: url(https://s3.amazonaws.com/files.lbry.io/cover-home2.jpg)"></div>
+<div class="bg-image-full" style="background-color: white" <?php //style="background-image: url(https://s3.amazonaws.com/files.lbry.io/cover-home2.jpg)" ?>></div>
 <?php Response::setMetaTitle(__('title.home')) ?>
 <?php Response::setMetaDescription(__('description.home')) ?>
-<?php echo View::render('nav/_header', ['isDark' => true]) ?>
-<main class="column-fluid">
-  <div class="span12">
-    <div class="cover cover-dark">
-      <div class="content content-wide content-dark">
-        <div class="text-center">
-          <h1 class="cover-title">{{global.tagline}}</h1>
-          <h2 class="cover-subtitle" style="max-width: 600px; margin-left: auto; margin-right: auto">{{global.sentence}}</h2>
-        </div>
-        <div class="control-group spacer2 text-center">
-          <div class="control-item">
-            <a href="/get" class="btn-primary">{{global.get}}</a>
-          </div>
-          <div class="control-item">
-            <a href="/learn" class="btn-alt">{{global.learn}}</a>
-          </div>
-        </div>
-        <div class="video" style="margin-bottom: 80px">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/DjouYBEkQPY" frameborder="0" allowfullscreen></iframe>
-        </div>
-      </div>
-      <div class="content content-dark">
-        <div class="row-fluid">
-          <div class="span4">
-            <h3><?php echo __('email.updates') ?></h3>
-            <?php echo View::render('mail/_subscribeForm', ['submitLabel' => __('email.go'), 'btnClass' => 'btn-alt']) ?>
-          </div>
-          <div class="span4 text-center">
-            <div class="fb-page" data-href="https://www.facebook.com/lbryio" data-height="300" data-small-header="false" data-width="300"
-                 data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true">
-              <div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/lbryio"><a href="https://www.facebook.com/lbryio">LBRY</a></blockquote></div>
-            </div>
-          </div>
-          <div class="span4 text-center">
-            <a width="300" class="twitter-timeline" href="https://twitter.com/LBRYio" data-widget-id="671104143034073088">{{social.tweets}}</a>
-          </div>
-        </div>
-      </div>
+<?php echo View::render('nav/_header', ['isDark' => false, 'isBordered' => false]) ?>
+<main class="home">
+  <div class="cover home__title cover-light">
+    <h1 class="cover-title cover-title-flat text-center">Content Freedom</h1>
+  </div>
+  <div class="cover home__media">
+    <img alt="Picture of LBRY Browser" src="/img/lbry-ui.png" />
+  </div>
+  <div class="cover cover-light content content-light content-wide home__copy">
+    <div class="spacer2 home">
+      <h2 class="cover-subtitle cover-title-flat">LBRY is a free, open, and community-run digital marketplace.</h2>
+      <h3 class="cover-subtitle cover-title-flat">You own your data. You control the network. Indeed, you <em>are</em> the network.</h3>
+      <h3 class="cover-subtitle cover-title-flat">Hollywood films, college lessons, amazing streamers and more are on the first media network ruled by <em>you</em>.</h3>
+    </div>
+    <div class="spacer2 text-center">
+      <a href="/get" class="btn-primary btn-large">Early Access</a>
+      <a href="/learn" class="btn-link btn-large">{{global.learn}}</a>
     </div>
   </div>
 </main>
+<?php echo View::render('nav/_footer', ['isDark' => false, 'isBordered' => false]) ?>
--- view/template/page/learn.php
+++ view/template/page/learn.php
@@ -1,117 +1,57 @@
 <?php Response::setMetaDescription(__('description.learn')) ?>
 <?php Response::setMetaTitle(__('title.learn')) ?>
 <?php echo View::render('nav/_header', ['isDark' => false]) ?>
-  <main class="column-fluid ">
-    <div class="span6">
-      <div class="cover cover-dark cover-dark-grad">
-        <div class="content content-dark content-tile">
-          <h1 class="cover-title cover-title-tile">{{learn.100}}</h1>
-          <?php echo View::render('download/_videoIntro') ?>
-        </div>
-      </div>
-    </div>
-    <div class="span6">
-      <div class="cover cover-dark cover-center content content-dark" style="background-image:url(/img/altamira-bison.jpg)">
-        <h2 class="cover-title cover-title-tile">{{learn.art}}</h2>
-        <p class="cover-subtitle text-center" style="max-width: 660px">{{learn.how}}</p>
-        <a href="/what" class="btn-alt"><?php echo __('learn.essay') ?></a>
-      </div>
-    </div>
-    <div class="span4">
-      <div class="cover cover-light-alt cover-light-alt-grad">
-        <div class="content content-light content-tile">
-          <h3>{{learn.explore}}</h3>
-          <div class="spacer1">
-            <a href="/news" class="link-primary">Latest News</a>
-          </div>
-          <div class="spacer1">
-            <a href="/faq" class="link-primary">{{page.faq.header}}</a>
-          </div>
-          <div class="spacer1">
-            <a href="http://explorer.lbry.io" class="link-primary">{{learn.explorer}}</a>
-          </div>
-          <div class="spacer1">
-            <a href="/team" class="link-primary">About the Team</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="span4">
-      <div class="cover cover-light">
-        <div class="content content-light content-tile">
-          <h3>{{learn.nerd}}</h3>
-          <p>LBRY is 100% open source in the <a class="link-primary" href="https://en.wikipedia.org/wiki/The_Cathedral_and_the_Bazaar">Bazaar tradition</a>.</p>
-          <?php echo View::render('social/_listDev') ?>
-        </div>
-      </div>
-    </div>
-    <div class="span4">
-      <div class="cover cover-dark cover-dark-grad">
-        <div class="content content-dark content-tile">
-          <h3>{{learn.join}}</h3>
-          <?php echo View::render('social/_list') ?>
-        </div>
-      </div>
-    </div>
-  </main>
-<?php /*
-<main class="column-fluid">
-  <div class="span4">
-    <div class="cover cover-column cover-light-alt cover-light-alt-grad">
-      <div class="content content-light">
-        <h1><?php echo __('What?') ?></h1>
-        <div class="spacer1">
-          <div class="spacer1">
-            <a href="/img/lbry-ui.png">
-              <img src="/img/lbry-ui.png" alt="Screenshot of a LBRY browser"/>
-            </a>
-          </div>
-          <p><em>Puts on jargon hat.</em></p>
-          <p>
-            LBRY is a decentralized, censorship-resistant, open-source, peer-to-peer information marketplace and discovery protocol.
-          </p>
-          <p><em>Removes jargon hat.</em></p>
-          <p>
-            LBRY is a new way for people to publish and share content with each other.
-          </p>
-          <p>
-            Our goal is to provide a single box that allows anyone anywhere to find and purchase digital content from anyone else.
-          </p>
-        </div>
-        <a href="/what" class="btn-primary"><?php echo __('More About LBRY') ?></a>
+<main class="column-fluid ">
+  <div class="span6">
+    <div class="cover cover-dark cover-dark-grad">
+      <div class="content content-dark content-tile">
+        <h1 class="cover-title cover-title-tile">{{learn.100}}</h1>
+        <?php echo View::render('download/_videoIntro') ?>
       </div>
     </div>
   </div>
+  <div class="span6">
+    <div class="cover cover-dark cover-center content content-dark" style="background-image:url(/img/altamira-bison.jpg)">
+      <h2 class="cover-title cover-title-tile">{{learn.art}}</h2>
+      <p class="cover-subtitle text-center" style="max-width: 660px">{{learn.how}}</p>
+      <a href="/what" class="btn-alt"><?php echo __('learn.essay') ?></a>
+    </div>
+  </div>
   <div class="span4">
-    <div class="cover  cover-column  cover-dark cover-dark-grad ">
-      <div class="content content-dark">
-        <h1><?php echo __('Why?') ?></h1>
+    <div class="cover cover-light-alt cover-light-alt-grad">
+      <div class="content content-light content-tile">
+        <h3>{{learn.explore}}</h3>
         <div class="spacer1">
-          <p><?php echo __('Current systems benefit huge corporations that add little but extract a lot.') ?></p>
-          <p>
-            <?php echo __('We don\'t like it when middlemen, greedy rent-seekers, and kleptocrats win.') ?></p>
-          </p>
-          <p><?php echo __('We think a better world is one in which artists and consumers are directly connected.') ?></p>
+          <a href="/news" class="link-primary">Latest News</a>
         </div>
         <div class="spacer1">
-          <a href="/why" class="btn-alt"><?php echo __('Why Make LBRY') ?></a>
+          <a href="/faq" class="link-primary">{{page.faq.header}}</a>
         </div>
-        <div>
-          <img src="/img/smbc-comic.png" />
+        <div class="spacer1">
+          <a href="http://explorer.lbry.io" class="link-primary">{{learn.explorer}}</a>
         </div>
-        <div class="meta text-center">
-          Credit <a href="//www.smbc-comics.com/" class="link-primary">SMBC</a>.
+        <div class="spacer1">
+          <a href="/team" class="link-primary">About the Team</a>
         </div>
       </div>
     </div>
   </div>
   <div class="span4">
-    <div class="cover  cover-column cover-light">
-      <div class="content">
-
-        <img src="/img/cover-team.jpg" alt="<?php echo __('LBRY Founders') ?>" />
+    <div class="cover cover-light">
+      <div class="content content-light content-tile">
+        <h3>{{learn.nerd}}</h3>
+        <p>LBRY is 100% open source in the <a class="link-primary" href="https://en.wikipedia.org/wiki/The_Cathedral_and_the_Bazaar">Bazaar tradition</a>.</p>
+        <?php echo View::render('social/_listDev') ?>
       </div>
     </div>
   </div>
-</main> */ ?>
+  <div class="span4">
+    <div class="cover cover-dark cover-dark-grad">
+      <div class="content content-dark content-tile">
+        <h3>{{learn.join}}</h3>
+        <?php echo View::render('social/_list') ?>
+      </div>
+    </div>
+  </div>
+</main>
 <?php echo View::render('nav/_footer') ?>
