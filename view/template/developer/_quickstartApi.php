e158d9aafddd744839add39423e18068e62c86d1
Fillerino <fillerix@fillerix.se>
2017-06-07 22:39:09 +0200

    Updated the quickstart to be more Windows friendly
    Updated the quickstart to be more Windows friendly( added powershell example code and how to use it).

--- view/template/developer/_quickstartApi.php
+++ view/template/developer/_quickstartApi.php
@@ -3,7 +3,7 @@
   When running, the LBRY daemon provides a JSON-RPC server running at <code class="code-inline">http://localhost:5279/lbryapi</code>.
 </p>
 <p>
-  It can be accessed by any utility capable of making HTTPS GET and POST requests, such as cURL or possibly your toaster.
+  It can be accessed by any utility capable of making HTTPS GET and POST requests, such as cURL or possibly your toaster. Psst, if you are on windows you could use PowerShell while testing, scroll down to the bottom of this page to get more info.
 </p>
 <p>
   To verify the LBRY daemon is running correctly, let's try looking up a URI:
@@ -53,4 +53,9 @@
 <p>The LBRY API consists of about 50 calls, all related to discovering, distributing, and purchasing content. <a class="link-primary" href="/api">View the full API documentation</a>.</p>
 <p>You can also list all of the commands available by calling the <span class="code-plan">help</span> command.</p>
 <code class="code-bash"><span class="code-bash__prompt">$</span>curl 'http://localhost:5279/lbryapi' --data '{"method":"help"}'
+</code>
+<h3>Windows</h3>
+<p>If you are running Windows and would like to follow this guide you could substitute curl with a PowerShell console and the following code.
+</p>
+<code class="code-bash"><span class="code-bash__prompt">$</span>Invoke-RestMethod -Uri 'http://localhost:5279/lbryapi' -Body 'THE_JSON_DATA' -Method POST  | ConvertTo-Json
 </code>
