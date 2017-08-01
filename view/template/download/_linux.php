7834d79540fa3e6601a150b27e032849a0f40c71
Jeremy Kauffman <jeremy@lbry.io>
2017-08-01 17:03:30 -0400

    fix broken linux download page

--- view/template/download/_linux.php
+++ view/template/download/_linux.php
@@ -2,9 +2,8 @@
   <p>
     <a id="linux-download" class="btn-alt btn-large" <?php echo $downloadUrl ? 'download' : '' ?>
        href="<?php echo $downloadUrl ?: 'https://github.com/lbryio/lbry-app/releases' ?>"
-      <?php 
-         data-facebook-track-id="1618717031725766" /*
-         data-twitter-track-id="XXXXX"           */ ?>
+         data-facebook-track-id="1618717031725766"   
+   <?php  /*         data-twitter-track-id="XXXXX"           */ ?>
          data-analytics-category="Sign Up"
          data-analytics-action="Download"
          data-analytics-label="Linux"
