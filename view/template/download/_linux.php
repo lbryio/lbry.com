491a8eafc02374967a55261ec0c26dc026bf6fb5
Josh Finer <josh@lbry.io>
2017-08-01 16:48:47 -0400

    Update _linux.php

--- view/template/download/_linux.php
+++ view/template/download/_linux.php
@@ -2,8 +2,8 @@
   <p>
     <a id="linux-download" class="btn-alt btn-large" <?php echo $downloadUrl ? 'download' : '' ?>
        href="<?php echo $downloadUrl ?: 'https://github.com/lbryio/lbry-app/releases' ?>"
-      <?php /*
-         data-facebook-track-id="XXXXX"
+      <?php 
+         data-facebook-track-id="1618717031725766" /*
          data-twitter-track-id="XXXXX"           */ ?>
          data-analytics-category="Sign Up"
          data-analytics-action="Download"
