bbdb173ba74d6b33658c2ec4b8f85e8cd27abac2
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2017-09-13 16:31:23 -0400

    temporary 404 page

--- view/template/page/404.php
+++ view/template/page/404.php
@@ -1,8 +1,7 @@
 <?php echo View::render('nav/_header', ['isDark' => false]) ?>
 <main>
   <div class="content">
-    <h1>{{page.notfound}}</h1>
-    <p>{{page.funnier}}</p>
+    <img src="img/LBRY-404-temp.png" alt="404" style="width: 100%;">
     <?php echo View::render('nav/_errorFooter') ?>
   </div>
 </main>
