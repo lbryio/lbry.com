afb2908509f10682ba7b412d8fe11f3823e1cc95
Jeremy Kauffman <jeremy@lbry.io>
2017-09-26 16:54:07 -0400

    user-controlled 404

--- view/template/page/404.php
+++ view/template/page/404.php
@@ -1,7 +1,16 @@
 <?php echo View::render('nav/_header', ['isDark' => false]) ?>
 <main>
   <div class="content">
-    <img src="img/LBRY-404-temp.png" alt="404" style="width: 100%;">
-    <?php echo View::render('nav/_errorFooter') ?>
+    <h1>Page Not Found (404)</h1>
+    <p>
+      The requested page could not be found. Here is the image located at lbry://404 to console you.
+    </p>
+    <p>
+      <img src="https://spee.ch/404.png" alt="404" style="width: 100%;">
+    </p>
+    <div class="meta">
+      The above image is not controlled by LBRY, Inc. Instead, it is the most popular claim for the URL lbry://404.
+      Want to change it? <a href="/get">Download LBRY</a>.
+    </div>
   </div>
 </main>
