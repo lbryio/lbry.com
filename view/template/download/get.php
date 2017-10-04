aa7aae2e91e204bf38a1cab2bba099cc00c6549c
Jeremy Kauffman <kauffj@gmail.com>
2017-10-04 18:44:02 -0400

    add missing timestamp

--- view/template/download/get.php
+++ view/template/download/get.php
@@ -30,7 +30,7 @@
             <span class="meta">
               <?php echo $version ?>,
               <?php echo number_format($size, 1) ?>MB,
-              built on <?php echo date('F d') ?>
+              built on <?php echo date('F d', $releaseTimestamp) ?>
               at <?php echo date('H:i:s', $releaseTimestamp) ?>
             </span>
           </p>
