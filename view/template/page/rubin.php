81d823b5b3dfa032634e4860f949ea89ec35aa92
Reilly Smith <reilly@lbry.io>
2017-12-04 15:35:13 -0800

    copy updates

--- view/template/page/rubin.php
+++ view/template/page/rubin.php
@@ -11,7 +11,7 @@
           Dave gets paid.*
         </h1>
         <div class="spacer1">
-          <a href="/get" class="btn-primary btn-large">Download Now</a>
+          <a href="/get" class="btn-primary btn-large">Watch Rubin on LBRY</a>
         </div>
         <div class="meta">
           *Only Dave. No middlemen.
@@ -22,14 +22,14 @@
   <div class="span5">
     <div class="cover cover-light">
       <div class="content content-light content-readable">
-        <h3>What's All This?</h3>
-        <p>LBRY is a departure from the platforms of yesterday. Support shows like The Rubin Report with 100% of your donations, subscriptions, and attention. Just you, and Dave.</p>
-        <h3>Right. And I Have A Bridge To Sell You.</h3>
-        <p>Welcome to the magic of blockchain. LBRY is open-source, decentralized, and shaped entirely by the creators and community who use it.</p>
-        <h3>This Is A 3rd Title</h3>
-        <p>There are lots of nifty things about how LBRY works (pronounced, "library").</p>
+        <h3>What am I looking at here?</h3>
+        <p>LBRY is a departure from the platforms of yesterday. Support shows like The Rubin Report with 100% of your donations, subscriptions, and attention.</p>
+        <p>Just you, and Dave.</p>
+        <h3>Okay. But why?</h3>
+        <p>Open-source and decentralized, LBRY is shaped entirely by the creators and community who use it. <b>Free speech and censorship-resistance are baked into the design.</b></p>
+        <p>There are lots of nifty aspects to how LBRY works (pronounced, "library"). Learn from the old-timers on our Discord at https://chat.lbry.io</p>
         <div class="text-center">
-          <a href="/get" class="btn-alt btn-large">Download Now</a>
+          <a href="/get" class="btn-alt btn-large">Download the App</a>
         </div>
       </div>
     </div>
