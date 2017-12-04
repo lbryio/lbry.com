e2fd851f4a788580ee4601dad41e9c118889d518
Jeremy Kauffman <kauffj@gmail.com>
2017-12-04 16:35:51 -0500

    rubin as 2 columns

--- view/template/page/rubin.php
+++ view/template/page/rubin.php
@@ -1,45 +1,36 @@
 <?php Response::setMetaDescription(__('description.learn')) ?>
 <?php Response::setMetaTitle(__('title.learn')) ?>
 <?php Response::addJsAsset('https://cdn.optimizely.com/js/9525271075.js') ?>
-<?php echo View::render('nav/_header', ['isDark' => false]) ?>
-<main class="column-fluid ">
-  <div class="span12">
-    <div class="cover cover-dark cover-center">
-      <div class="content">
-        <h1 class="cover-title cover-title-tile cover-title-flat">Dave creates. We watch.
-<br/>
-         Dave gets paid.*
+<?php echo View::render('nav/_header', ['isDark' => false, 'isAbsolute' => false]) ?>
+<main class="column-fluid">
+  <div class="span7">
+    <div class="cover cover-dark cover-center cover-full" style="background-image:url(/img/dave-phillyd-background.png)">
+      <div class="content content-dark text-center">
+        <h1 class="cover-title cover-title-tile cover-title-flat">
+          Dave creates. We watch.<br/>
+          Dave gets paid.*
         </h1>
-        <p class="text-center">LBRY is a departure from the platforms of yesterday. Support shows like The Rubin Report with 100% of your donations, subscriptions, and attention. Just you, and Dave.</p>
-</div>
-    </div>
-
-   <div class="content content-light content-readable">
-    <img src="/img/dave-phillyd-background.png" alt="Image of Dave and PhillyD" />
-       <a href="/get" class="btn-alt btn-large">Download Now</a>
-         <p class="text-center" style="max-width: 800px">It's open-source, decentralized, and shaped entirely by the creators who use it.</p>
-         <p class="text-center" style="max-width: 800px">There are lots of nifty things about how LBRY works (pronounced, "library"). You can learn a lot from the old-timers on our Discord at https://chat.lbry.io if you're feeling adventurous.</p>
-         <p class="text-center" style="max-width: 800px">*Only Dave. Not middlemen.</p>
-    </div>
-  </div>
-
-  <div class="span6">
-    <div class="cover cover-dark cover-dark-grad">
-      <div class="content content-dark content-tile">
-        <h3>{{learn.join}}</h3>
-          <div class="spacer1">
-            <a href="/faq" class="link-primary">{{page.faq.header}}</a>
-            (<a href="/faq/what-is-lbry" class="link-primary">What is LBRY?</a>)
-          </div>
-        <?php echo View::render('social/_list') ?>
+        <div class="spacer1">
+          <a href="/get" class="btn-primary btn-large">Download Now</a>
+        </div>
+        <div class="meta">
+          *Only Dave. No middlemen.
+        </div>
       </div>
     </div>
   </div>
-  <div class="span6">
-    <div class="cover cover-light-alt cover-light-alt-grad">
-      <div class="content content-light content-tile">
-        <h3>For Developers</h3>
-        <?php echo View::render('social/_listDev') ?>
+  <div class="span5">
+    <div class="cover cover-light">
+      <div class="content content-light content-readable">
+        <h3>What's All This?</h3>
+        <p>LBRY is a departure from the platforms of yesterday. Support shows like The Rubin Report with 100% of your donations, subscriptions, and attention. Just you, and Dave.</p>
+        <h3>Right. And I Have A Bridge To Sell You.</h3>
+        <p>Welcome to the magic of blockchain. LBRY is open-source, decentralized, and shaped entirely by the creators and community who use it.</p>
+        <h3>This Is A 3rd Title</h3>
+        <p>There are lots of nifty things about how LBRY works (pronounced, "library").</p>
+        <div class="text-center">
+          <a href="/get" class="btn-alt btn-large">Download Now</a>
+        </div>
       </div>
     </div>
   </div>
