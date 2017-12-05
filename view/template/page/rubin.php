aa213e99a8010d06927c4ac79c45d41f23d5b573
Reilly Smith <reilly@lbry.io>
2017-12-04 16:13:39 -0800

    Update rubin.php

--- view/template/page/rubin.php
+++ view/template/page/rubin.php
@@ -14,6 +14,8 @@
         </br>
         </br>
         </br>
+        </br>
+        </br>
         <div class="spacer1">
           <a href="/get" class="btn-primary btn-large">Watch Rubin on LBRY</a>
         </div>
