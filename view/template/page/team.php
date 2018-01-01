6322070ee91a1710cdd89a8b123d75ddff959f7b
Jeremy Kauffman <kauffj@gmail.com>
2018-01-01 11:10:00 -0500

    fix meta image on team page

--- view/template/page/team.php
+++ view/template/page/team.php
@@ -1,5 +1,5 @@
 <?php NavActions::setNavUri('/learn') ?>
-<?php Response::addMetaImage(Request::getHostAndProto() . '/img/cover-team.jpg') ?>
+<?php Response::addMetaImage('https://spee.ch/@lbryteam/everyone.jpg') ?>
 <?php Response::setMetaDescription('description.team') ?>
 <?php echo View::render('nav/_header', ['isDark' => false]) ?>
 <main>
