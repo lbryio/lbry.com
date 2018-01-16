074449dbe2ec427c982030a6237c92eac18c308e
Josh Finer <josh@lbry.io>
2018-01-16 10:33:06 -0500

    google optimize anti-flicker code

--- view/template/layout/_analytics.php
+++ view/template/layout/_analytics.php
@@ -1,4 +1,10 @@
 <?php if (IS_PRODUCTION): ?>
+<style>.async-hide { opacity: 0 !important} </style>
+<script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+})(window,document.documentElement,'async-hide','dataLayer',4000,
+{'GTM-NT8579P':true});</script>
   <script>
     <?php //google analytics ?>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
