3be19879b96d372c7f3c003b59863fb6358911c8
Aaron Muir Hamilton <aaron@correspondwith.me>
2018-02-03 22:17:37 +0000

    dev.sh: Run `php` if there is no `php7.0` in PATH

--- dev.sh
+++ dev.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PHPBIN=php7.0
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
@@ -7,4 +9,8 @@ if [ ! -e "data/config.php" ]; then
   cp "$DIR/data/config.php.example" "$DIR/data/config.php"
 fi
 
-php7.0 --server localhost:8000 --docroot "$DIR/web" "$DIR/web/index.php"
+if ! which $PHPBIN 2>/dev/null; then
+    PHPBIN=php
+fi
+
+$PHPBIN --server localhost:8000 --docroot "$DIR/web" "$DIR/web/index.php"
