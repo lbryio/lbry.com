75b146f223fbe2ec0a1408e00263e5179e9abe01
Maxime St-Pierre <me@maximest-pierre.me>
2018-02-04 08:34:23 -0500

    Add composer install to docker.sh

--- docker.sh
+++ docker.sh
@@ -13,4 +13,5 @@ docker run --rm -it --name "dev.lbry.io" \
   -p "127.0.0.1:8000:8000" \
   -u "$(id -u):$(id -g)" \
   php:7-alpine \
+  php composer.phar install
   php --server "0.0.0.0:8000" --docroot "web/" "index.php"
