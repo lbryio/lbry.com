kafene/lbry.io

Add category filter for news post

controller/Controller.class.php
@@ -125,6 +125,7 @@ class Controller
     $router->any('/yt2', 'AcquisitionActions::executeYT2');
     $router->get('/verify/{token}', 'AcquisitionActions::executeVerify');
 
+    $router->get('/news/category/{category}', 'ContentActions::preparePostCategoryFilter');
 
     $router->post('/set-culture', 'i18nActions::setCulture');
 

lbryio/lbry.com

controller/Controller.class.php
@@ -125,6 +125,7 @@ class Controller
     $router->any('/yt2', 'AcquisitionActions::executeYT2');
     $router->get('/verify/{token}', 'AcquisitionActions::executeVerify');
 
+    $router->get('/news/category/{category}', 'ContentActions::executePostCategoryFilter');
 
     $router->post('/set-culture', 'i18nActions::setCulture');
 