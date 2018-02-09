2473c4b38f57a9fb3ff321f7b59f992d5b79cfbc
Jeremy Curcio <j.curcio@me.com>
2018-02-09 13:14:29 -0500

    Send Linux OSes to /linux with the button.
    Signed-off-by: Jeremy Curcio <j.curcio@me.com>

--- view/template/download/_button.php
+++ view/template/download/_button.php
@@ -1,36 +1,29 @@
 <?php if (isset($downloadUrl)): ?>
-  <p>
-    Securely download the LBRY app here, and see what all the fuss is about!
-  </p>
-  <p>
-  </p>
-  <div class="text-center">
-    <p>
-      <a class="btn-alt btn-large"
-         download
-         href="<?php echo $downloadUrl ?>"
-         id="get-download-btn"
-         data-facebook-track="1"
-         data-analytics-category="Sign Up"
-         data-analytics-action="Download"
-         data-analytics-label="<?php echo $analyticsLabel ?>"
-      ><?php echo $buttonLabel ?></a>
-      <?php if ($isAuto): ?>
-        <?php js_start() ?>
-          var anchor = document.getElementById('get-download-btn');
-          ga('send', 'event', anchor.getAttribute('data-analytics-category'), anchor.getAttribute('data-analytics-action'), anchor.getAttribute('data-analytics-label'));
-          setTimeout(function() { window.location = anchor.getAttribute('href'); }, 500);
-        <?php js_end() ?>
-      <?php endif ?>
-      <br/>
-      <span class="meta">
-        <?php echo $version ?>,
-        <?php echo number_format($size, 1) ?>MB,
-        built on <?php echo date('F d', $releaseTimestamp) ?>
-        at <?php echo date('H:i:s', $releaseTimestamp) ?>
-      </span>
-    </p>
-  </div>
-<?php else: ?>
+   <a class="btn-alt btn-large"
+     download
+     href="<?php echo $downloadUrl ?>"
+     id="get-download-btn"
+     data-facebook-track="1"
+     data-analytics-category="Sign Up"
+     data-analytics-action="Download"
+     data-analytics-label="<?php echo $analyticsLabel ?>"
+  ><?php echo $buttonLabel ?></a>
+  <?php if ($isAuto): ?>
+    <?php js_start() ?>
+      var anchor = document.getElementById('get-download-btn');
+      ga('send', 'event', anchor.getAttribute('data-analytics-category'), anchor.getAttribute('data-analytics-action'), anchor.getAttribute('data-analytics-label'));
+      setTimeout(function() { window.location = anchor.getAttribute('href'); }, 500);
+    <?php js_end() ?>
+  <?php endif ?>
+  <br/>
+  <span class="meta">
+    <?php echo $version ?>,
+    <?php echo number_format($size, 1) ?>MB,
+    built on <?php echo date('F d', $releaseTimestamp) ?>
+    at <?php echo date('H:i:s', $releaseTimestamp) ?>
+  </span>
+<?php elseif ($os === 'linux') : ?>
+  <a href="/linux" class="btn-primary btn-large spacer1">Download LBRY</a><BR>  
+<?php else : ?>
   <a href="/get" class="btn-primary btn-large spacer1">Download LBRY</a><BR>
 <?php endif ?>
