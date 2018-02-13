6f09f09646dcf1b0985d83749ef5c8c3f03133a3
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2018-02-13 18:09:28 -0500

    update discord link + open in new window

--- view/template/social/_listDev.php
+++ view/template/social/_listDev.php
@@ -10,11 +10,11 @@
   <a href="/api" class="link-primary"><span class="icon-code icon-fw"></span><span class="btn-label">The API</span></a>
 </div>
 <div class="spacer1">
-  <a href="https://github.com/lbryio" class="link-primary"><span class="icon-code-fork icon-fw"></span><span class="btn-label">Source Code</span></a>
+  <a href="https://github.com/lbryio" target="_blank" class="link-primary"><span class="icon-code-fork icon-fw"></span><span class="btn-label">Source Code</span></a>
 </div>
 <div class="spacer1">
   <a href="/roadmap" class="link-primary"><span class="icon-road icon-fw"></span><span class="btn-label">Roadmap</span></a>
 </div>
 <div class="spacer1">
-  <a href="https://discord.gg/YjYbwhS" class="link-primary"><span class="icon-comments icon-fw"></span><span class="btn-label">Discord (#dev channel)</span></a>
+  <a href="https://chat.lbry.io" target="_blank" class="link-primary"><span class="icon-comments icon-fw"></span><span class="btn-label">Discord (#dev channel)</span></a>
 </div>
