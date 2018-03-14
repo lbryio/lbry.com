8ea1ae0f2b70114c11877bfe40f9f68e797f3d7b
YULIUS KURNIAWAN KRISTIANTO <yuliuskristianto9@gmail.com>
2018-03-15 03:50:51 +0700

    underline download lbry
    so user can click link to redirect /get

--- view/template/page/404.php
+++ view/template/page/404.php
@@ -10,7 +10,7 @@
     </p>
     <div class="meta">
       The above image is not controlled by LBRY, Inc. Instead, it is the most popular claim for the URL lbry://404.
-      Want to change it? <a href="/get">Download LBRY</a>.
+      Want to change it? <u><a href="/get">Download LBRY</a></u>.
     </div>
   </div>
 </main>
