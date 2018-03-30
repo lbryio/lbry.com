a053d9048424898a62d4b83cbf5f035305cd913b
Brinck Slattery <Brinck@lbry.io>
2018-03-30 15:32:04 -0400

    Update android-alpha.php

--- view/template/page/android-alpha.php
+++ view/template/page/android-alpha.php
@@ -20,7 +20,7 @@
       <h3>Want to help build the first ever decentralized, blockchain based media streaming app?</h3>
       <p> We're looking for developers who can help us get our Android app ready for prime time.</p>
       <h3>How do I get involved?</h3>
-      <p>Enter your email below. We'll put you in our developer queue (please note that you will <b>not</b> receive an email right away) and send you everything you need to get started when you reach the top.</p>
+      <p>Enter your email below. We'll put you in our testing queue (please note that you will <b>not</b> receive an email right away) and send you everything you need to get started when you reach the top.</p>
       <?php echo View::render('mail/_subscribeForm', [
         'tag' => 'android-alpha',
         'submitLabel' => 'Make Me an Alpha Tester!',
