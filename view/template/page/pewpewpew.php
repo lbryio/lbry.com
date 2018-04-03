b94d576a42baefba69fd2f73611c15c9fa755191
Brinck Slattery <Brinck@lbry.io>
2018-04-03 16:10:59 -0400

    Update pewpewpew.php

--- view/template/page/pewpewpew.php
+++ view/template/page/pewpewpew.php
@@ -13,8 +13,18 @@
 
 <p>And to those who think they can restrict our free speech? Molṑn labé.</p>
 
+      <img src="https://spee.ch/5a3e08d52dd2d7cb1c63a480b45dea8b4679cf01/lbryget-gif-mastertest.gif" />
+
 <p>You can <a href="/get?src=FA">download the LBRY app here.</a> If you have any questions or need help, <a href="http://chat.lbry.io">join our Discord community.</a></p>
-<div class="text-center"><a href="/get?src=FA" class="btn-primary btn-large">Get LBRY App</a></div>      
+<div class="text-center"><a href="/get?src=FA" class="btn-primary btn-large">Get LBRY App</a></div>
+      <h3>Want to hear from us when we get new firearms-related content? Sign up for our mailing list below.</h3>
+      <?php echo View::render('mail/_subscribeForm', [
+        'tag' => 'guns',
+        'submitLabel' => 'Keep Me In The Loop',
+        'hideDisclaimer' => true,
+        'largeInput' => true,
+        'btnClass' => 'btn-alt btn-large',
+      ]) ?>
     </div>
   </div>
 </main>
