b95cb8a6642a17f19a8a66d7315f6b6cc86f5771
Brinck Slattery <Brinck@lbry.io>
2018-04-05 10:37:51 -0400

    Update speech-admin.php

--- view/template/page/speech-admin.php
+++ view/template/page/speech-admin.php
@@ -21,7 +21,7 @@
       <p>Fine.</p>
       <p>spee.ch is a free, open-source web portal for LBRY content. It both publishes content to the LBRY network and serves data from the LBRY network, but it does it over the web for improved usability.</p>
       <p>Recently, spee.ch has been re-engineered to support self-hosting and custom skinning, as well as hosting only a portion of the network. You could run a spee.ch fork that's dedicated specifically to your favorite cat and has the appearance to show it.</p>
-      <h3This is where you come in.</h3>
+      <h3>This is where you come in.</h3>
       <p>This functionality is brand new and we're offering LBC bounties to those who can help us test and refine it. If you're capable of installing Wordpress, you're probably capable of installing spee.ch</p>
       <p>So if you want to play around with a funky new technology, contribute to content freedom, and earn weird internet tokens, join us for an introductory session! We'll explain more about the program and walk you through the basics of how spee.ch works and how to set it up.</p>
       <?php echo View::render('mail/_subscribeForm', [
