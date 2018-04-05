a225e36e7c4eea80bfb17bfa23f229498fe07b52
Brinck Slattery <Brinck@lbry.io>
2018-04-05 11:54:56 -0400

    Update canary.php

--- view/template/page/canary.php
+++ view/template/page/canary.php
@@ -4,7 +4,7 @@
     <div class="content">
       <h1>LBRY Warrant Canary</h1>
       <p>
-        Through January 1st, 2018, LBRY has received:
+        Through April 1st, 2018, LBRY has received:
       </p>
       <ul>
         <li>Zero National Security Letters</li>
