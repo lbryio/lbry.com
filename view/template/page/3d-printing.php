01bb22f4b038e726acec6ab81358542f24f2cf4c
James Biller <james@lbry.io>
2018-05-01 12:02:12 -0400

    3D Printing Landing Page
    This is a rough draft of the landing page that I'll be sending the 3D printing community (from sites like Thingiverse) to.

---
+++ view/template/page/3d-printing.php
@@ -0,0 +1,51 @@
+<?php Response::setMetaDescription(__('description.meetup')) ?>
+<?php Response::setMetaTitle(__('title.meetup')) ?>
+<?php echo View::render('nav/_header', ['isDark' => true, 'isAbsolute' => true]) ?>
+
+<main>
+  <div class="cover cover-dark cover-center cover-full cover--dark-overlay" style="background-image: url(/img/3d-crypto-accepted-here.jpg)">
+    <div class="content content-dark text-center">
+      <h1 class="cover-title cover-title-tile cover-item--outline">
+        3D printing on the blockchain
+      </h1>
+      <h3 class="cover-item--outline">
+      </h3>
+      <div class="spacer1">
+        <a href="#about" class="btn-primary btn-large">Tell Me More!</a>
+      </div>
+    </div>
+  </div>
+  <div class="cover cover-light" id="about">
+    <div class="content content-light content-readable">
+      <h3>Wanna earn crypto for your 3D files?</h3>
+      <p> Now you can buy and sell files at ANY PRICE or share your files for free & earn microtips!</p>
+      <p> Like Bitcoin, LBRY (pronounced library) uses blockchain technology, enabling you to
+      earn money without a bank or 3rd party.<p>
+      <h3>Sounds futuristic. How do I start earning?</h3>
+      <p>You can <a href="/get?src=FA">download the LBRY app here.</a> If you have any questions or need help, <a href="http://chat.lbry.io">join our Discord community.</a></p>
+<div class="text-center spacer1"><a href="/get?src=FA" class="btn-primary btn-large">Get LBRY App</a></div>
+
+    </div>
+    <body align="center">
+    <h1> Paste the links into the LBRY App to download! </h1>
+      <img src="https://i.imgur.com/rvdMLB2.jpg" alt=Bitcoin-Accepted-Here style="width:500px;height:500px;">
+        <h4> Bitcoin Accepted Here Tabletop Sign </h4>
+          <p> lbry://@3dprinting#5aab622139e1fad2518c48fbead755a9f0e3f73a/bitcoinacceptedhere </p>
+      <img src="https://spee.ch/f/Ethereum-Accepted-Here-Sign.jpeg" alt=Ethereum-Accepted-Here style="width:500px;height:500px;">
+        <h4> Ethereum Accepted Here Tabletop Sign </h4>
+          <p> lbry://@3dprinting#5aab622139e1fad2518c48fbead755a9f0e3f73a/ethereumacceptedhere </p>
+      <img src="https://spee.ch/c/Bitcoin-Cash-Accepted-Here.jpeg" alt=Bitcoin-Cash-Accepted-Here style="width:500px;height:500px;">
+        <h4> Bitcoin Cash Accepted Here Tabletop Sign </h4>
+          <p> lbry://@3dprinting#5aab622139e1fad2518c48fbead755a9f0e3f73a/bitcoincashtray </p>
+      <img src="https://spee.ch/a/Monero-Accepted-Here.jpeg" alt=Monero-Accepted-Here style="width:500px;height:500px;">
+        <h4> Monero Accepted Here Tabletop Sign </h4>
+          <p> lbry://@3dprinting#5aab622139e1fad2518c48fbead755a9f0e3f73a/moneroacceptedhere </p>
+      <img src="https://spee.ch/5/Dash-Accepted-Here.jpeg" alt=Dash-Accepted-Here style="width:500px;height:500px;">
+        <h4> Dash Accepted Here Tabletop Sign </h4>
+          <p> lbry://@3dprinting#5aab622139e1fad2518c48fbead755a9f0e3f73a/dashacceptedhere </p>
+      <img src="https://spee.ch/c/LBRY-Credits-Accepted-Here.jpeg" alt=LBRY-Credits-Accepted-Here style="width:500px;height:500px;">
+        <h4> LBRY Credits Accepted Here Tabletop Sign </h4>
+          <p> lbry://@3dprinting#5aab622139e1fad2518c48fbead755a9f0e3f73a/lbrycreditsacceptedhere </p>
+      </body>
+  </div>
+</main>
