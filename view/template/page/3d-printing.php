d620a65428ec105e8c1258c99f11c272bf3f2b4d
James Biller <james@lbry.io>
2018-05-07 15:15:46 -0400

    Deleted height
    Deleted height to make images not distort

--- view/template/page/3d-printing.php
+++ view/template/page/3d-printing.php
@@ -21,7 +21,7 @@
       <p> Now you can buy and sell files at ANY PRICE or share your files for free & earn MICROTIPS!</p>
       <p> Like Bitcoin, LBRY (library) uses blockchain technology, enabling you to
       earn without a bank or 3rd party.<p>
-        <img src="https://spee.ch/9/BitcoinMeets3Dprinting-01.png" alt=Bitcoin-Meets-3D-Printing style="width:750px;height:250px;">
+        <img src="https://spee.ch/9/BitcoinMeets3Dprinting-01.png" alt=Bitcoin-Meets-3D-Printing style="width:750px;">
       <h3>Sounds futuristic. How do I start earning?</h3>
       <p>You can <a href="/get?src=FA">download the LBRY app here.</a> If you have any questions or need help, <a href="http://chat.lbry.io">join our Discord community.</a></p>
 <div class="text-center spacer1"><a href="/get?src=FA" class="btn-primary btn-large">Get LBRY App</a></div>
@@ -30,11 +30,11 @@
     <body align="center">
     <h3> Print your first file from the blockchain!</h3>
     <h4> Open the app & click a sign to download.</h4>
-      <a href="https://open.lbry.io/bitcoinacceptedhere"> <img src="https://spee.ch/9/BitcoinAcceptedHereTransparent.png" alt=Bitcoin-Accepted-Here style="width:500px;height:500px;"></a>
-      <a href="https://open.lbry.io/EthereumAcceptedHere"> <img src="https://spee.ch/1/EthereumAcceptedHereTransparent.png" alt=Ethereum-Accepted-Here style="width:500px;height:500px;"></a>
-      <a href="https://open.lbry.io/BitcoinCashacceptedhere"> <img src="https://spee.ch/e/BitcoinCashAcceptedHereTransparent.png" alt=Bitcoin-Cash-Accepted-Here style="width:500px;height:500px;"></a>
-      <a href="https://open.lbry.io/MoneroAcceptedHere"> <img src="https://spee.ch/1/MoneroAcceptedHereTransparent.png" alt=Monero-Accepted-Here style="width:500px;height:500px;"></a>
-      <a href="https://open.lbry.io/DashAcceptedHere"> <img src="https://spee.ch/d/DashAcceptedHereTransparent2.png" alt=Dash-Accepted-Here style="width:500px;height:500px;"></a>
-      <a href="https://open.lbry.io/LBRYCreditsAcceptedHere"> <img src="https://spee.ch/f/LBRY-Credits-Accepted-Here-Transparent.png" alt=Dash-Accepted-Here style="width:500px;height:500px;"></a>
+      <a href="https://open.lbry.io/bitcoinacceptedhere"> <img src="https://spee.ch/9/BitcoinAcceptedHereTransparent.png" alt=Bitcoin-Accepted-Here style="width:500px;"></a>
+      <a href="https://open.lbry.io/EthereumAcceptedHere"> <img src="https://spee.ch/1/EthereumAcceptedHereTransparent.png" alt=Ethereum-Accepted-Here style="width:500px;"></a>
+      <a href="https://open.lbry.io/BitcoinCashacceptedhere"> <img src="https://spee.ch/e/BitcoinCashAcceptedHereTransparent.png" alt=Bitcoin-Cash-Accepted-Here style="width:500px;"></a>
+      <a href="https://open.lbry.io/MoneroAcceptedHere"> <img src="https://spee.ch/1/MoneroAcceptedHereTransparent.png" alt=Monero-Accepted-Here style="width:500px;"></a>
+      <a href="https://open.lbry.io/DashAcceptedHere"> <img src="https://spee.ch/d/DashAcceptedHereTransparent2.png" alt=Dash-Accepted-Here style="width:500px;"></a>
+      <a href="https://open.lbry.io/LBRYCreditsAcceptedHere"> <img src="https://spee.ch/f/LBRY-Credits-Accepted-Here-Transparent.png" alt=Dash-Accepted-Here style="width:500px;"></a>
   </div>
 </main>
