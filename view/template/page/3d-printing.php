616acfa753a0e4027e23f922284166f799806430
Electron - Mark Firth <bananabox100@gmail.com>
2018-05-09 18:05:55 +1000

    Cleaner Here Link
    Moved the period out of the link.

--- view/template/page/3d-printing.php
+++ view/template/page/3d-printing.php
@@ -15,7 +15,7 @@
         <p>Like Bitcoin, LBRY (library) uses blockchain technology, enabling you to earn without a bank or 3rd party.</p>
         <p><img src="https://spee.ch/0/BitcoinMeets3Dprinting.jpeg" alt="Bitcoin-Meets-3D-Printing" width="667px" height="250"></p>
       <h2>Sounds Futuristic. How Do I Start Earning?</h2>
-        <p>You can download the LBRY app <a href="/get?src=FA">here.</a> If you have any questions or need help,join our <a href="http://chat.lbry.io">Discord</a> community.</p>
+        <p>You can download the LBRY app <a href="/get?src=FA">here</a>. If you have any questions or need help,join our <a href="http://chat.lbry.io">Discord</a> community.</p>
       <span class="text-center spacer1"><a href="/get?src=FA" class="btn-primary btn-large">DOWNLOAD THE LBRY APP</a></span> </div>
   </section>
   <section>
