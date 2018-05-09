7269f12a2fa1cee5b67e1cabb4d8506539f6a99a
Electron - Mark Firth <bananabox100@gmail.com>
2018-05-09 15:41:29 +1000

    Update 3d-printing.php
    Fixed: Images sizes and file type.
    
    Images for the site should be much much smaller and jpg for these types of images unless transparency is needed.
    
    **PNG**: Block graphics such as logos.
    **JPG**: Gradient graphics such as photos.
    
    New images are jpeg compressed and resized to 500x500, sRGB, progressive.
    
    Typical size for an image of **500px by 500px** would be something like **10kb-35kb.** previous images were in excess of 2.3mb each. New image sizes are around 20kb.
    
    Large images would not work well on mobile devices and would have a heavy **impact** on **search engine ranking**. And browser resizing can make them look pixelated.
    
    ![image](https://user-images.githubusercontent.com/29914179/39794758-50bf97ea-538f-11e8-8765-d95943beec0e.png).
    
    Alt tags corrected. Had missing "" and Title tags added.
    
    Corrected LBRY Alt tag which set as Dash.
    Correctly nested HTags
    Used Sections where appropriate.
    Corrected spelling grammer mistakes.
    Converted CSS width/height to HTML.
    
    *If H1, H2, H3 styling isn't the look you are going for, use CSS to style rather than using incorrectly nested H Tags.
    
    I typically use Bootstrap for standard base styles, then use a separate custom css file on top to be more flexible in various areas where needed.

--- view/template/page/3d-printing.php
+++ view/template/page/3d-printing.php
@@ -1,40 +1,31 @@
+
 <?php Response::setMetaDescription(__('description.3d-printing')) ?>
 <?php Response::setMetaTitle(__('title.3d-printing')) ?>
 <?php echo View::render('nav/_header', ['isDark' => true, 'isAbsolute' => true]) ?>
 
-<main>
-  <div class="cover cover-dark cover-center cover-full cover--dark-overlay" style="background-image: url(/img/3d-crypto-accepted-here.jpg)">
+<main style="text-align:center;">
+  <section class="cover cover-dark cover-center cover-full cover--dark-overlay" style="background-image: url(/img/3d-crypto-Accepted-Here.jpg)">
     <div class="content content-dark text-center">
-      <h1 class="cover-title cover-title-tile cover-item--outline">
-        3D printing on the blockchain
-      </h1>
-      <h3 class="cover-item--outline">
-      </h3>
-      <div class="spacer1">
-        <a href="#about" class="btn-primary btn-large">Tell Me More!</a>
-      </div>
+      <h1 class="cover-title cover-title-tile cover-item--outline">3D Printing On The Blockchain</h1>
+      <div class="spacer1"><a href="#about" class="btn-primary btn-large">Tell Me More!</a></div>
     </div>
-  </div>
-  <div class="cover cover-light" id="about">
-    <div class="content content-light content-readable">
-      <h3>Wanna earn crypto for your 3D files?</h3>
-      <p> Now you can buy and sell files at ANY PRICE or share your files for free & earn MICROTIPS!</p>
-      <p> Like Bitcoin, LBRY (library) uses blockchain technology, enabling you to
-      earn without a bank or 3rd party.<p>
-        <img src="https://spee.ch/9/BitcoinMeets3Dprinting-01.png" alt=Bitcoin-Meets-3D-Printing style="width:750px;">
-      <h3>Sounds futuristic. How do I start earning?</h3>
-      <p>You can <a href="/get?src=FA">download the LBRY app here.</a> If you have any questions or need help, <a href="http://chat.lbry.io">join our Discord community.</a></p>
-<div class="text-center spacer1"><a href="/get?src=FA" class="btn-primary btn-large">Get LBRY App</a></div>
-
-    </div>
-    <body align="center">
-    <h3> Print your first file from the blockchain!</h3>
-    <h4> Open the app & click a sign to download.</h4>
-      <a href="https://open.lbry.io/bitcoinacceptedhere"> <img src="https://spee.ch/9/BitcoinAcceptedHereTransparent.png" alt=Bitcoin-Accepted-Here style="width:500px;"></a>
-      <a href="https://open.lbry.io/EthereumAcceptedHere"> <img src="https://spee.ch/1/EthereumAcceptedHereTransparent.png" alt=Ethereum-Accepted-Here style="width:500px;"></a>
-      <a href="https://open.lbry.io/BitcoinCashacceptedhere"> <img src="https://spee.ch/e/BitcoinCashAcceptedHereTransparent.png" alt=Bitcoin-Cash-Accepted-Here style="width:500px;"></a>
-      <a href="https://open.lbry.io/MoneroAcceptedHere"> <img src="https://spee.ch/1/MoneroAcceptedHereTransparent.png" alt=Monero-Accepted-Here style="width:500px;"></a>
-      <a href="https://open.lbry.io/DashAcceptedHere"> <img src="https://spee.ch/d/DashAcceptedHereTransparent2.png" alt=Dash-Accepted-Here style="width:500px;"></a>
-      <a href="https://open.lbry.io/LBRYCreditsAcceptedHere"> <img src="https://spee.ch/f/LBRY-Credits-Accepted-Here-Transparent.png" alt=Dash-Accepted-Here style="width:500px;"></a>
-  </div>
-</main>
+    <div class="cover cover-light content content-light content-readable" id="about" >
+      <h2>Wanna Earn Crypto For Your 3D Files?</h2>
+        <p>Now you can buy and sell files at <strong>any price</strong> or share your files for free &amp; earn <strong>microtips</strong>!</p>
+        <p>Like Bitcoin, LBRY (library) uses blockchain technology, enabling you to earn without a bank or 3rd party.</p>
+        <p><img src="https://spee.ch/0/BitcoinMeets3Dprinting.jpeg" alt="Bitcoin-Meets-3D-Printing" height="250"></p>
+      <h2>Sounds Futuristic. How Do I Start Earning?</h2>
+        <p>You can download the LBRY app <a href="/get?src=FA">here.</a> If you have any questions or need help,join our <a href="http://chat.lbry.io">Discord</a> community.</p>
+      <span class="text-center spacer1"><a href="/get?src=FA" class="btn-primary btn-large">DOWNLOAD THE LBRY APP</a></span> </div>
+  </section>
+  <section>
+      <h2>Print Your First File From The Blockchain!</h2>
+        <p><strong>Open the app &amp; click a sign to download.</strong></p>
+	        <a href="https://open.lbry.io/bitcoinacceptedhere"> <img style="padding:20px;" src="https://spee.ch/e/bitcoin3d.jpeg" alt="Bitcoin Accepted Here" title="Bitcoin Accepted Here" width="500px" height="500px"></a>
+	        <a href="https://open.lbry.io/EthereumAcceptedHere"> <img style="padding:20px;" src="https://spee.ch/e/ethereum3d.jpeg" alt="Ethereum Accepted Here" title="Ethereum Accepted Here" width="500px" height="500px"></a>
+	        <a href="https://open.lbry.io/BitcoinCashacceptedhere"> <img style="padding:20px;" src="https://spee.ch/a/bitcoincash3d.jpeg" alt="Bitcoin-Cash Accepted Here" title="Bitcoin-Cash Accepted Here" width="500px" height="500px"></a>
+	        <a href="https://open.lbry.io/MoneroAcceptedHere"> <img style="padding:20px;" src="https://spee.ch/3/monero3d.jpeg" alt="Monero Accepted Here" title="Monero Accepted Here" width="500px" height="500px"></a>
+	        <a href="https://open.lbry.io/DashAcceptedHere"> <img style="padding:20px;" src="https://spee.ch/5/dassh3d.jpeg" alt="Dash Accepted Here" title="Dash Accepted Here" width="500px" height="500px"></a>
+	        <a href="https://open.lbry.io/LBRYCreditsAcceptedHere"> <img style="padding:20px;" src="https://spee.ch/0/lbry3d.jpeg" alt="LBRY Accepted Here" title="LBRY Accepted Here" width="500px" height="500px"></a>
+  </section>
+</main
