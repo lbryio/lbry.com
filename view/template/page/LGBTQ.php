41045c4d25ac21533c42370e53681bed88e20d48
Brinck Slattery <Brinck@lbry.io>
2018-06-04 19:17:27 -0400

    Update LGBTQ.php

--- view/template/page/LGBTQ.php
+++ view/template/page/LGBTQ.php
@@ -1,5 +1,5 @@
 <?php Response::setMetaDescription(__('YouTube is demonetizing and burying LGBTQ videos - try a platform that\'s proud to have your content')) ?>
-<?php Response::setMetaTitle(__('You\'re Here, You\'re Queer, We want to get used to it')) ?>
+<?php Response::setMetaTitle(__('You\'re Here, You\'re Queer, We\'re Cool With It')) ?>
 <?php echo View::render('nav/_header', ['isDark' => true, 'isAbsolute' => true]) ?>
 <main>
   <div class="cover cover-dark cover-center cover-full cover--dark-overlay" style="background-image: url(/img/LGBTQ.png)">
@@ -23,12 +23,12 @@
       <p>Our system is decentralized - we care about our users, not advertisers trying to make a buck off of their hard work.</p>
       <h3>This is where you come in.</h3>
       <p>We're building LBRY for everyone, and we want you to join us! As long as it's legal, we'll never tell our users what they can and can't talk about.</p>
-      <p>If you're willing to give cutting edge technology (that may not work 100% of the time) a try, download the LBRY app and let us know what you think! </p>
-       <img src="https://spee.ch/5a3e08d52dd2d7cb1c63a480b45dea8b4679cf01/lbryget-gif-mastertest.gif" />
-
-<p>You can <a href="/get?src=FA">download the LBRY app here.</a> If you have any questions or need help, <a href="http://chat.lbry.io">join our Discord community.</a></p>
-<div class="text-center spacer1"><a href="/get?src=FA" class="btn-primary btn-large">Get LBRY App</a></div>
-
+      <h1>Do you have a favorite LGBTQ creator?</h1>
+      <p>Send them this page and ask them to sync their content today with our YouTube Partner Program.</p>
+        
+       <div class="spacer2 text-center">
+<a class="btn-primary btn-large" href="https://lbry.io/youtube">Join our Partner Program</a>
+</div>
       <p>If you'd like to get information and updates when new LGBTQ content goes live on our network and hear what's happening in the fight for free speech online, sign up for our mailing list below.</p>
     
       <?php echo View::render('mail/_subscribeForm', [
