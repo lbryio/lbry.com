9a551c0c6f57bfec2212adb75643804d783c0670
Kristian Polso <kristian.polso@gmail.com>
2018-06-09 19:52:19 +0300

    Closes #597

--- view/template/layout/basic.php
+++ view/template/layout/basic.php
@@ -54,6 +54,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
         <base target="_parent" />
+
+        <?php echo View::render('layout/_analytics_head') ?>
     </head>
     <body>
       <?php echo $content ?>
@@ -66,7 +68,7 @@
           <?php foreach (Response::getJsAssets() as $src): ?>
             <script src="<?php echo $src ?>"></script>
           <?php endforeach ?>
-          <?php echo View::render('layout/_analytics') ?>
+          <?php echo View::render('layout/_analytics_footer') ?>
           <?php $js = Response::getJsCalls() ?>
           <?php if ($js): ?>
             <script><?php echo implode("\n", $js) ?></script>
