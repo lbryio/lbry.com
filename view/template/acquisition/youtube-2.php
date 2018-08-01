60fb7358af1038f8e2d0eeb5758995f77b2c0820
YULIUS KURNIAWAN KRISTIANTO <yuliuskristianto9@gmail.com>
2018-08-01 22:54:16 +0700

    update link

--- view/template/acquisition/youtube-2.php
+++ view/template/acquisition/youtube-2.php
@@ -44,7 +44,7 @@ Response::setMetaDescription("Put your content on the blockchain, experience tru
       <div class="zigzag"></div>
       <h1>Sync &amp; Earn Crypto</h1>
       <p>LBRY offers a single-click sync process<br>for existing YouTubers</p>
-        <form class="form" id="sync" method="post" action="http://api.lbry.io/yt/connect">
+        <form class="form" id="sync" method="post" action="https://lbry.io/youtube">
             <div class="form-inner">
               <div class="block">
                 <div class="center">
