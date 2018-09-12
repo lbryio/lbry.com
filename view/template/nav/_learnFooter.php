ce28e188e663d304abf706c0a3d106afaf786909
YULIUS KURNIAWAN KRISTIANTO <yuliuskristianto9@gmail.com>
2018-09-12 20:02:19 +0700

    move explore and add new link to new line (#752)
    based @jsigwart commented on my closed PR a day ago...

--- view/template/nav/_learnFooter.php
+++ view/template/nav/_learnFooter.php
@@ -17,12 +17,13 @@
           <li><a href="/list/subscribe" class="link-primary"><?php echo __('email.subscribe') ?></a></li>
           <li>Join us on <a href="https://twitter.com/lbryio" target="_blank" class="link-primary"><span class="btn-label">Twitter</span><span class="icon icon-twitter"></span></a>
             <a href="https://facebook.com/lbryio" target="_blank" class="link-primary"><span class="btn-label">Facebook</span><span class="icon icon-facebook"></span></a>
-            <a href="https://reddit.com/r/lbry" target="_blank" class="link-primary"><span class="btn-label">Reddit</span><span class="icon icon-reddit"></span></a>
             <a href="https://chat.lbry.io" target="_blank" class="link-primary"><span class="btn-label">Discord</span><span class="icon-comments icon-fw"></span></a>
 	    <a href="https://www.instagram.com/lbryio/" target="_blank" class="link-primary"><span class="btn-label">Instagram</span><span class="fab fa-instagram icon-fw"></span></a>
             <a href="https://t.me/lbryofficial" target="_blank" class="link-primary"><span class="btn-label">Telegram</span><span class="fab fa-telegram icon-fw"></span></a> 
-       or explore our <a href="https://shop.lbry.io" target="_blank" class="link-primary"><span class="btn-label">LBRY Shop</span><span class="icon-fw icon-shopping-cart"></span></a></li>        
-        </ul>
+          <li>Explore our <a href="https://shop.lbry.io" target="_blank" class="link-primary"><span class="btn-label">LBRY Shop</span><span class="icon-fw icon-shopping-cart"></span></a>
+       <a href="https://lbry.fund/" target="_blank" class="link-primary"><span class="btn-label">LBRY Community Fund</span><span class="fas fa-hand-holding-usd icon-fw"></span></a>
+       <a href="https://lbry.io/3d-printing" target="_blank" class="link-primary"><span class="btn-label">3D Printing Program</span><span class="icon-fw icon-cube"></span></a>
+             </ul>
       </div>
       <div class="span6">
         <h3><?php echo __('publish.keepl') ?></h3>
