503160bd4fe7908d375b6630ea4d0b604c1f812b
netop:// ウェッブ <netopwibby@thenetwork.email>
2018-09-21 15:17:24 -0500

    Emphasized usage of LBRY app (#777)
    * Emphasized usage of LBRY app
    
    added text to help android users. This will eventually go away when we move verification to lbry.io directly.

--- view/template/acquisition/verify.php
+++ view/template/acquisition/verify.php
@@ -1,46 +1,55 @@
 <!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-  <title>Verify Your Identity</title>
-  <link rel="icon" href="images/favicon.ico">
-  <script src='https://www.google.com/recaptcha/api.js'></script>
-  <script type="text/javascript">
-      var magicLink = "#";
-      var verifyCallback = function(response) {
-        let payload = btoa(JSON.stringify({
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Verify Your Identity</title>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+
+    <link rel="icon" href="images/favicon.ico"/>
+
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+
+    <script>
+      let magicLink = "#";
+
+      const verifyCallback = response => {
+        const payload = btoa(JSON.stringify({
           recaptcha: response,
           token: "<?php echo $token ?>"
         }));
+
         magicLink = "lbry://?verify=" + payload;
+
         document.getElementById("magic-link-text").textContent = payload;
         document.getElementById("success").style.display = "block";
         document.getElementById("captcha-block").style.display = "none";
       };
-      var expiredCallback = function() {
+
+      const expiredCallback = () => {
         document.getElementById("success").style.display = "none";
         document.getElementById("captcha-block").style.display = "block";
       }
-  </script>
-</head>
-<body>
-  <div style="display: flex; align-items: center; flex-direction: column;" class="text-center">
-    <img src="/img/lbry-dark-1600x528.png" style="max-height: 80px; margin-top: 50px;" alt="LBRY"/>
-    <h1>Almost Done!</h1>
-    <div id="captcha-block">
-      <p>Click the captcha to continue...</p>
-      <br/>
-      <div class="g-recaptcha" data-sitekey="6LcG_z0UAAAAAKBPDBhiJU_jI9cRNRiJwcUHq95u" data-callback="verifyCallback" data-expired-callback="expiredCallback"></div>
+    </script>
+  </head>
+
+  <body>
+    <div style="display: flex; align-items: center; flex-direction: column;" class="text-center">
+      <img src="/img/lbry-dark-1600x528.png" style="max-height: 80px; margin-top: 50px;" alt="LBRY"/>
+      <h1>Almost Done!</h1>
+      <div id="captcha-block">
+        <p>Click the captcha to continue...</p>
+        <br/>
+        <div class="g-recaptcha" data-sitekey="6LcG_z0UAAAAAKBPDBhiJU_jI9cRNRiJwcUHq95u" data-callback="verifyCallback" data-expired-callback="expiredCallback"></div>
+      </div>
+      <div style="display: none; margin-top: 10px;" id="success">
+        <p>Now click the magic link below to verify your identity in the LBRY app...</p>
+        <br/>
+        <a class="btn-primary btn-large spacer1" onclick="location.href=magicLink">Magic Link</a>
+        <p><i>When prompted, open the link with with LBRY Browser on Android or LBRY on Desktop. Does the magic link not work? Not on the same device as the LBRY app? Paste this (very long) piece of text into the verification screen of the app to confirm your identity.</i></p>
+        <code class="multiline-code" id="magic-link-text"></code>
+      </div>
     </div>
-    <div style="  display: none; margin-top: 10px;" id="success">
-      <p>Now click the magic link below to verify your identity in app...</p>
-      <br/>
-      <a class="btn-primary btn-large spacer1" onclick="location.href=magicLink">Magic Link</a>
-      <p><i>Does the magic link not work? Not on the same device as the app? Paste this (very long) piece of text into the verification screen of the app to confirm your identity.</i></p>
-      <code class="multiline-code" id="magic-link-text"></code>
-    </div>
-  </div>
-</body>
+  </body>
 </html>
