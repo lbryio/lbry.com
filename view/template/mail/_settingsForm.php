5b8ca112d0924c45062c99a1951fa7925be8ec88
ポール ウェッブ <netopwibby@thenetwork.email>
2018-09-21 09:57:16 -0500

    Styling fixes for email subscription page

--- view/template/mail/_settingsForm.php
+++ view/template/mail/_settingsForm.php
@@ -11,8 +11,44 @@
     <div class="notice notice-error spacer1"><?php echo $error ?></div>
   <?php endif ?>
   <div class="notice notice-success hide">Your email preferences have been updated.</div>
+
+  <section class="tag-section spacer1">
+    <h4>Fine-tune your newsletter</h4>
+    <div class="notice notice-error hide"></div>
+
+    <div class="row-fluid spacer1">
+      <?php $tagIndex = 0 ?>
+      <?php foreach($tags as $tag => $enabled): ?>
+      <?php $tagId = 'tag_' . (++$tagIndex) ?>
+      <div class="span6">
+        <div class="row-fluid">
+          <div class="span10">
+            <label for="<?php echo $tagId ?>"><?php echo isset($tagMetadata[$tag]['label']) ? $tagMetadata[$tag]['label'] : $tag ?></label>
+          </div>
+
+          <div class="span2">
+            <span class="slider-checkbox">
+              <input id="<?php echo $tagId ?>" type="checkbox" <?php echo $enabled ? 'checked' : '' ?> value="<?php echo urlencode($tag) ?>" />
+              <label class="label"></label>
+            </span>
+          </div>
+        </div>
+
+        <?php if (isset($tagMetadata[$tag]['description'])): ?>
+        <div class="meta">
+          <?php echo $tagMetadata[$tag]['description'] ?>
+        </div>
+        <?php endif ?>
+      </div>
+      <?php if ($tagIndex % 2 == 0): ?>
+      </div><div class="row-fluid spacer1">
+      <?php endif ?>
+      <?php endforeach ?>
+    </div>
+  </section>
+
   <section class="email-section">
-    <h4>Where do you want to receive email?</h4>
+    <h4>Quick Unsubscribe</h4>
     <div class="notice notice-error hide spacer1"></div>
     <?php $emailIndex = 0 ?>
     <table>
@@ -35,38 +71,10 @@
         </tr>
       <?php endforeach ?>
     </table>
+
+    <div class="meta">If you toggle "off" and hit "Save", you will be removed from all of our mailing lists.</div>
   </section>
-  <section class="tag-section spacer1">
-    <h4>What do you want to receive email about?</h4>
-    <div class="notice notice-error hide"></div>
-    <div class="row-fluid spacer1">
-      <?php $tagIndex = 0 ?>
-      <?php foreach($tags as $tag => $enabled): ?>
-        <?php $tagId = 'tag_' . (++$tagIndex) ?>
-        <div class="span6">
-          <div class="row-fluid">
-            <div class="span10">
-              <label for="<?php echo $tagId ?>"><?php echo isset($tagMetadata[$tag]['label']) ? $tagMetadata[$tag]['label'] : $tag ?></label>
-            </div>
-            <div class="span2">
-              <span class="slider-checkbox">
-                <input id="<?php echo $tagId ?>" type="checkbox" <?php echo $enabled ? 'checked' : '' ?> value="<?php echo urlencode($tag) ?>" />
-                <label class="label"></label>
-              </span>
-            </div>
-          </div>
-          <?php if (isset($tagMetadata[$tag]['description'])): ?>
-            <div class="meta">
-              <?php echo $tagMetadata[$tag]['description'] ?>
-            </div>
-          <?php endif ?>
-        </div>
-        <?php if ($tagIndex % 2 == 0): ?>
-          </div><div class="row-fluid spacer1">
-        <?php endif ?>
-      <?php endforeach ?>
-    </div>
-  </section>
+
   <div>
     <input type="submit" value="Save" class="btn-primary">
   </div>
