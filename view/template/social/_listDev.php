0bac9d580850844c39b9a37512cef924657de2e5
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2018-10-11 23:19:55 -0400

    Update _listDev.php

--- view/template/social/_listDev.php
+++ view/template/social/_listDev.php
@@ -7,7 +7,7 @@
   <a href="https://github.com/lbryio" target="_blank" class="link-primary"><span class="icon-code-fork icon-fw"></span><span class="btn-label">Source Code</span></a>
 </div>
 <div class="spacer1">
-  <a href="https://lbry.tech" class="link-primary"><span class="icon-fast-forward icon-fw"></span><span class="btn-label">lbry.tech</span></a>
+  <a href="https://lbry.tech" class="link-primary"><span class="icon-cog icon-fw"></span><span class="btn-label">lbry.tech</span></a>
 </div>
 <div class="spacer1">
   <a href="https://lbry.tech/contribute" class="link-primary"><span class="icon-question icon-fw"></span><span class="btn-label">Contributor's Guide</span></a>
