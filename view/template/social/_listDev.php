2af4a7aba266b5d13b63a70175bbccb4caf95d1f
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2018-10-11 23:16:16 -0400

    Update _listDev.php
    remove quickstart and roadmap, add lbry.tech

--- view/template/social/_listDev.php
+++ view/template/social/_listDev.php
@@ -4,19 +4,16 @@
 </div>
  */ ?>
 <div class="spacer1">
-  <a href="/quickstart" class="link-primary"><span class="icon-fast-forward icon-fw"></span><span class="btn-label">Quickstart</span></a>
-</div>
-<div class="spacer1">
-  <a href="/api" class="link-primary"><span class="icon-code icon-fw"></span><span class="btn-label">The API</span></a>
-</div>
-<div class="spacer1">
   <a href="https://github.com/lbryio" target="_blank" class="link-primary"><span class="icon-code-fork icon-fw"></span><span class="btn-label">Source Code</span></a>
 </div>
 <div class="spacer1">
-  <a href="/roadmap" class="link-primary"><span class="icon-road icon-fw"></span><span class="btn-label">Roadmap</span></a>
+  <a href="https://lbry.tech" class="link-primary"><span class="icon-fast-forward icon-fw"></span><span class="btn-label">lbry.tech</span></a>
 </div>
 <div class="spacer1">
   <a href="https://lbry.tech/contribute" class="link-primary"><span class="icon-question icon-fw"></span><span class="btn-label">Contributor's Guide</span></a>
+</div>
+<div class="spacer1">
+  <a href="https://discourse.lbry.io" target="_blank" class="link-primary"><span class="icon-comments-o icon-fw"></span><span class="btn-label">LBRY Tech Forum</span></a>
 </div>
 <div class="spacer1">
   <a href="https://chat.lbry.io" target="_blank" class="link-primary"><span class="icon-comments icon-fw"></span><span class="btn-label">Discord (#dev channel)</span></a>
