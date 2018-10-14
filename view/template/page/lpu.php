c53d93f0b1f3e2f1fc6272a197606a421413fbc2
Julie Sigwart <jsigwart@gmail.com>
2018-10-13 21:57:33 -0700

    Create lpu.php

---
+++ view/template/page/lpu.php
@@ -0,0 +1,33 @@
+<?php Response::setMetaDescription(__('description.meetup')) ?>
+<?php Response::setMetaTitle(__('title.meetup')) ?>
+<?php echo View::render('nav/_header', ['isDark' => true, 'isAbsolute' => true]) ?>
+<main>
+  <div class="cover cover-dark cover-center cover-full cover--dark-overlay" style="background-image: url(/img/table2.jpg)">
+    <div class="content content-dark text-center">
+      <h1 class="cover-title cover-title-tile cover-item--outline">
+        Welcome LPU Blockchain Meetup Attendees
+      </h1>
+      <h3 class="cover-item--outline">
+        Thank you for your interest in LBRY. 
+      </h3>
+      <div class="spacer1">
+        <a href="#about" class="btn-primary btn-large">Tell Me More!</a>
+      </div>
+    </div>
+  </div>
+  <div class="cover cover-light" id="about">
+    <div class="content content-light content-readable">
+      <h3>LBRY is a free, open, and community-run digital marketplace.</h3>
+      <p>Please sign up to learn more about LBRY, get free LBC, and see how you can use and develop on the LBRy protocol.</p>
+      <p>Enter your email below and we'll tell you more.</p>
+      <?php echo View::render('mail/_subscribeForm', [
+        'tag' => 'lpu',
+        'submitLabel' => 'Sign me up',
+        'hideDisclaimer' => true,
+        'largeInput' => true,
+        'btnClass' => 'btn-alt btn-large',
+      ]) ?>
+    </div>
+  </div>
+</main>
+<?php echo View::render('nav/_footer') ?>
