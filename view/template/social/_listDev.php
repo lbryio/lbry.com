5b14621ec1c1827d18d53e62021d7d5e59dc609f
YULIUS KURNIAWAN KRISTIANTO <yuliuskristianto9@gmail.com>
2018-10-21 00:08:46 +0700

    bring back quickstart

--- view/template/social/_listDev.php
+++ view/template/social/_listDev.php
@@ -7,6 +7,9 @@
   <a href="https://github.com/lbryio" target="_blank" class="link-primary"><span class="icon-code-fork icon-fw"></span><span class="btn-label">Source Code</span></a>
 </div>
 <div class="spacer1">
+  <a href="https://lbry.tech/playground" class="link-primary"><span class="icon-fast-forward icon-fw"></span><span class="btn-label">Quickstart</span></a>
+</div>
+<div class="spacer1">
   <a href="https://lbry.tech" class="link-primary"><span class="icon-cog icon-fw"></span><span class="btn-label">lbry.tech</span></a>
 </div>
 <div class="spacer1">
