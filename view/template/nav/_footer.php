6221ae78ab1cf9b6046d0aa43f40448f34256929
YULIUS KURNIAWAN KRISTIANTO <yuliuskristianto9@gmail.com>
2018-10-31 07:29:15 +0700

    add mail helpdesk to footer and learn footer (#861)

--- view/template/nav/_footer.php
+++ view/template/nav/_footer.php
@@ -11,13 +11,16 @@
         </div>
         <?php echo View::render('nav/_globalItems') ?>
         <div class="control-item no-label-desktop">
-        <a href="https://shop.lbry.io" target="_blank"><span class="btn-label">LBRY Shop</span> <span class="icon-fw icon-shopping-cart"></span></a>
-        </div>
-        <div class="control-item no-label-desktop">
         <a href="https://t.me/lbryofficial" target="_blank"><span class="btn-label">Telegram</span> <i class="fab fa-telegram"></i></a>
         </div>
         <div class="control-item no-label-desktop">
         <a href="https://www.instagram.com/lbryio/" target="_blank"><span class="btn-label">Instagram</span> <i class="fab fa-instagram"></i></a>
+        </div>
+        <div class="control-item no-label-desktop">
+        <a href="mailto:hello@lbry.io" target="_blank"><span class="btn-label">Contact Us</span> <span class="icon-fw icon-envelope"></span></a>
+        </div>
+        <div class="control-item no-label-desktop">
+        <a href="https://shop.lbry.io" target="_blank"><span class="btn-label">LBRY Shop</span> <span class="icon-fw icon-shopping-cart"></span></a>
         </div>
         <div class="control-item">
           <a href="https://en.wikipedia.org/wiki/Free_Speech_Flag" class="footer-img-link">
