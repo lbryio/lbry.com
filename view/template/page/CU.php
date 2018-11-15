fa5b4b1c9ce09016711c4f75049e680f8d74293d
Clement <38915222+ceoger@users.noreply.github.com>
2018-11-15 04:42:54 +0000

    Update CU.php
    corrected typo

--- view/template/page/CU.php
+++ view/template/page/CU.php
@@ -5,7 +5,7 @@
   <div class="cover cover-dark cover-center cover-full cover--dark-overlay" style="background-image: url(/img/clement-vit.jpg)">
     <div class="content content-dark text-center">
       <h1 class="cover-title cover-title-tile cover-item--outline">
-        LBRY Welcomes Chandigahr University Students
+        LBRY Welcomes Chandigarh University Students
       </h1>
       <h3 class="cover-item--outline">We're excited to share about LBRY with you!
       </h3>
