e01f0afbc8b313bff49c336d9ecfd570aa550ed1
Clement <38915222+ceoger@users.noreply.github.com>
2018-11-15 05:18:59 +0000

    Update lpu.php
    Updated Landing Page Text Description For LPU

--- view/template/page/lpu.php
+++ view/template/page/lpu.php
@@ -1,14 +1,13 @@
-<?php Response::setMetaDescription(__('Welcome to LBRY, LPU students.')) ?>
-<?php Response::setMetaTitle(__('LBRY Welcomes LPU')) ?>
+<?php Response::setMetaDescription(__('LBRY Welcomes LPU')) ?>
+<?php Response::setMetaTitle(__('LBRY Welcomes LPU Students')) ?>
 <?php echo View::render('nav/_header', ['isDark' => true, 'isAbsolute' => true]) ?>
 <main>
   <div class="cover cover-dark cover-center cover-full cover--dark-overlay" style="background-image: url(/img/table2.jpg)">
     <div class="content content-dark text-center">
       <h1 class="cover-title cover-title-tile cover-item--outline">
-        Welcome LPU Blockchain Meetup Attendees
+        LBRY Welcomes LPU Students
       </h1>
-      <h3 class="cover-item--outline">
-        Thank you for your interest in LBRY. 
+      <h3 class="cover-item--outline">We're excited to share about LBRY with you!
       </h3>
       <div class="spacer1">
         <a href="#about" class="btn-primary btn-large">Tell Me More!</a>
@@ -17,11 +16,10 @@
   </div>
   <div class="cover cover-light" id="about">
     <div class="content content-light content-readable">
-      <h3>LBRY is a free, open, and community-run digital marketplace.</h3>
-      <p>Please sign up to learn more about LBRY, get free LBC, and see how you can use and develop on the LBRY protocol. You may download the LBRY app <a href="https://lbry.io/get">here.</a></p>
-      <p>Enter your email below and we'll tell you more.</p>
+      <h3>Enter your email address to receive LBRY updates, news, and information for India's LBRYians!</h3>
+      <p>As an open source project, our development and future relies on our communities around the world, and we're delighted to see our community growing in India. Please enter your email below and we'll give you instructions to redeem the LBC as mentioned by our team.</p>
       <?php echo View::render('mail/_subscribeForm', [
-        'tag' => 'lpu',
+        'tag' => 'LPU',
         'submitLabel' => 'Sign me up',
         'hideDisclaimer' => true,
         'largeInput' => true,
