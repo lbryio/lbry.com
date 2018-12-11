f5e11f38dbe546fc06a9ed1309d15a6314254a31
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2018-12-11 16:30:38 -0500

    OSX version note

--- view/template/download/_meta.php
+++ view/template/download/_meta.php
@@ -7,6 +7,9 @@
     <br/>
     Works with Ubuntu, Debian, or any distro with <code>apt</code> or <code>dpkg</code>.
     For other Linux flavors including Arch and Flatpak support or compiling from source, <a href="https://github.com/lbryio/lbry-desktop#install" class="link-primary">see our GitHub install page</a>.
+  <?php elseif ($os === OS::OS_OSX): ?>
+    <br/>
+    Works with with macOS version 10.12.4 (Sierra), and higher.
   <?php elseif ($sourceLink): ?>
     <br/>
     Like source code? Go <a href="https://github.com/lbryio/lbry-desktop" class="link-primary">here</a>.
