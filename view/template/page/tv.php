094e9395554653b2a84f7a1b7f16e780214c44a4
ポール ウェッブ <netopwibby@thenetwork.email>
2019-02-01 16:30:09 -0600

    Initial commit

---
+++ view/template/page/tv.php
@@ -0,0 +1,13 @@
+<main class="soon">
+  <section>
+    <h1><strong>lbry.tv</strong> is coming soon</h1>
+    <p>Sign up for updates and get notified as soon as we launch!</p>
+
+    <?php echo View::render('mail/_subscribeForm', [
+      'tag' => 'lbrytv',
+      'submitLabel' => 'Sign Me Up',
+    ]) ?>
+
+    <p>Go back to <strong><a href="/">lbry.io</a></strong>.</p>
+  </section>
+</main>
