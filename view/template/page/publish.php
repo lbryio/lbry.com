240429c952b54fbaadcab1f66dc051e911f135d5
netop://ウエハ <paul@webb.page>
2019-06-26 12:45:59 -0500

    Initial commit

---
+++ view/template/page/publish.php
@@ -0,0 +1,28 @@
+<?php Response::setMetaDescription('Why should you publish to LBRY?') ?>
+<?php Response::setMetaTitle(__('LBRY &middot; Why publish your content here?')) ?>
+
+<main class="ancillary">
+  <section class="hero hero--half-height">
+    <div class="inner-wrap inner-wrap--center-hero">
+      <h1>Why publish to LBRY?</h1>
+    </div>
+  </section>
+
+  <section>
+    <div class="inner-wrap">
+      <h2 id="">Our two-minute proposal</h2>
+      <p>Quite frankly, we're a breath of fresh air. Why <em>not</em> try LBRY and see if it works (for you)?</p>
+
+      <p>You might've done some research and rolled your eyes because we're yet <em>another</em> startup using Blockchain&trade; within our tech stack. Why should you care about that? Well, <strong>utilizing a blockchain allows us to be censorship resistant</strong>.</p>
+
+      <p>The benefits of blockchain + the freedom to be yourself. No middleman required.</p>
+
+      <p>As long as servers exist, so will your content. So, <em>forever</em>. The only thing you have to worry about is content creation and making your fans happy.</p>
+
+      <figure>
+        <img alt="Yerbil loves you. Love the yerbil." src="/img/gerbil-happy.png"/>
+        <figcaption>Yerbil tax</figcaption>
+      </figure>
+    </div>
+  </section>
+</main>
