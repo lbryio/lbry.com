d7f20e682734ad954c205ed87a1526067fa26c86
Brinck Slattery <Brinck@lbry.io>
2019-09-10 15:21:59 -0400

    Update canary.php

--- view/template/page/canary.php
+++ view/template/page/canary.php
@@ -9,7 +9,7 @@
 
   <section>
     <div class="inner-wrap">
-      <p>Through September 1st, 2019, LBRY has received:</p>
+      <p>Through October 1st, 2019, LBRY has received:</p>
 
       <ul>
         <li>Zero National Security Letters</li>
