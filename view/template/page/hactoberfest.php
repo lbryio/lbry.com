91659a77b6912b0e143c6e31afb3b37202f93d01
Julie Sigwart <jsigwart@gmail.com>
2019-09-26 12:37:39 -0700

    Create hactoberfest.php

---
+++ view/template/page/hactoberfest.php
@@ -0,0 +1,16 @@
+<?php Response::setMetaDescription('LBRY Celebrates Hacktoberfest all During October') ?>
+
+<main class="ancillary">
+  <section class="hero hero--half-height">
+    <div class="inner-wrap inner-wrap--center-hero">
+      <h1>Hacktoberfest is Coming!</h1>
+    </div>
+  </section>
+
+  <section>
+    <div class="inner-wrap">
+      <p><img alt="hacktoberfest1" src="https://spee.ch/@lbrynews:0/hacktoberfest1.jpeg" /></p>
+      <p>As an Open Source company, LBRY is excited to participate again in this year's Hactoberfest! Check back soon for information on how you can contribute, the cool swag we're giving out, and an activity schedule.</p>
+    </div>
+  </section>
+</main>
