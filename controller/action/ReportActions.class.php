3480ad4ea71c904d27bd5f850a164600d8407a6f
Robin C <me@taminoful.de>
2019-10-02 01:28:59 +0200

    Fix claimId not being passed to report form.
    Fixes issue #582.

--- controller/action/ReportActions.class.php
+++ controller/action/ReportActions.class.php
@@ -2,6 +2,42 @@
 
 class ReportActions extends Actions
 {
+
+    /* TODO: Investigate optional route parameters, maybe nullable type hints from php 7.1 help with that. */
+    public static function executeDmca() {
+        Response::setHeader(Response::HEADER_CROSS_ORIGIN, "*");
+        if (!Request::isPost()) {
+            return ['report/dmca'];
+        }
+
+        $values = [];
+        $errors = [];
+
+        foreach (['name', 'email', 'rightsholder', 'identifier'] as $field) {
+            $value = Request::getPostParam($field);
+
+            if (!$value) {
+                $errors[$field] = __('form_error.required');
+            } elseif ($field == 'email' && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                $errors[$field] = __('form_error.invalid');
+            }
+
+            $values[$field] = $value;
+        }
+
+        if (!$errors) {
+            $values['report_id'] = Encoding::base58Encode(random_bytes(6));
+            Mailgun::sendDmcaReport($values);
+            Session::setFlash('success', '<h3>Report Submitted</h3><p>We will respond shortly.</p><p>This ID for this report is <strong>' . $values['report_id'] . '</strong>. Please reference this ID when contacting us regarding this report.</p>');
+            return Controller::redirect(Request::getRelativeUri(), 303);
+        }
+
+        return ['report/dmca', [
+            'errors' => $errors,
+            'values' => $values
+        ]];
+    }
+
     public static function executeDmcaWithClaimId(string $claimId)
     {
 
