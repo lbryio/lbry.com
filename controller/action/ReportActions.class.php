fa75a9eaf8f1f1e66b9275bce46381b19c868fcc
Robin C <me@taminoful.de>
2019-10-04 23:37:50 +0200

    Delegate methods so DRY isn't that hurt.
    Since the router doesn't support optional routing and this application
    uses an EOL (End of Life) PHP version the only thing I was able to do
    was extracting duplicated code into private methods.

--- controller/action/ReportActions.class.php
+++ controller/action/ReportActions.class.php
@@ -3,34 +3,22 @@
 class ReportActions extends Actions
 {
 
-    /* TODO: Investigate optional route parameters, maybe nullable type hints from php 7.1 help with that. */
+    /**
+     * Handles all routing requests without a claimId as parameter.
+     * E.g. /dmca
+     *
+     * @return array
+     * @throws Exception
+     */
     public static function executeDmca() {
-        Response::setHeader(Response::HEADER_CROSS_ORIGIN, "*");
-        if (!Request::isPost()) {
-            return ['report/dmca'];
-        }
+
+        self::setResponseHeader();
+        self::redirectIfPostRequest(false, '');
 
         $values = [];
         $errors = [];
 
-        foreach (['name', 'email', 'rightsholder', 'identifier'] as $field) {
-            $value = Request::getPostParam($field);
-
-            if (!$value) {
-                $errors[$field] = __('form_error.required');
-            } elseif ($field == 'email' && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
-                $errors[$field] = __('form_error.invalid');
-            }
-
-            $values[$field] = $value;
-        }
-
-        if (!$errors) {
-            $values['report_id'] = Encoding::base58Encode(random_bytes(6));
-            Mailgun::sendDmcaReport($values);
-            Session::setFlash('success', '<h3>Report Submitted</h3><p>We will respond shortly.</p><p>This ID for this report is <strong>' . $values['report_id'] . '</strong>. Please reference this ID when contacting us regarding this report.</p>');
-            return Controller::redirect(Request::getRelativeUri(), 303);
-        }
+        self::validateForm($values, $errors);
 
         return ['report/dmca', [
             'errors' => $errors,
@@ -38,27 +26,50 @@ class ReportActions extends Actions
         ]];
     }
 
-    public static function executeDmcaWithClaimId(string $claimId)
-    {
+    /**
+     * Handles all routing requests with a claimId as routing parameter since the used router doesn't support optional parameters.
+     * E.g. /dmca/this-is-a-claim-id
+     *
+     * @param string $claimId
+     *
+     * @return array
+     * @throws Exception
+     */
+    public static function executeDmcaWithClaimId(string $claimId): array {
 
-        if (isset($claimId)) {
-            $claimId = htmlspecialchars($claimId);
-        }
+        $claimId = htmlspecialchars($claimId);
 
-        Response::setHeader(Response::HEADER_CROSS_ORIGIN, "*");
-        if (!Request::isPost()) {
-            return ['report/dmca', ['claimId' => $claimId]];
-        }
+        self::setResponseHeader();
+        self::redirectIfPostRequest(true, $claimId);
 
         $values = [];
         $errors = [];
+
+        self::validateForm($values, $errors);
+
+        return ['report/dmca', [
+            'errors' => $errors,
+            'values' => $values,
+            'claimId' => $claimId
+        ]];
+    }
+
+    /**
+     * @param array $values
+     * @param array $errors
+     *
+     * @return array
+     * @throws Exception
+     */
+    private static function validateForm(array $values, array $errors)
+    {
 
         foreach (['name', 'email', 'rightsholder', 'identifier'] as $field) {
             $value = Request::getPostParam($field);
 
             if (!$value) {
                 $errors[$field] = __('form_error.required');
-            } elseif ($field == 'email' && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            } elseif ($field === 'email' && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
                 $errors[$field] = __('form_error.invalid');
             }
 
@@ -72,10 +83,34 @@ class ReportActions extends Actions
             return Controller::redirect(Request::getRelativeUri(), 303);
         }
 
-        return ['report/dmca', [
-            'errors' => $errors,
-            'values' => $values,
-            'claimId' => $claimId
-        ]];
+    }
+
+    /**
+     * Sets the response header.
+     */
+    private static function setResponseHeader()
+    {
+        Response::setHeader(Response::HEADER_CROSS_ORIGIN, "*");
+    }
+
+    /**
+     * Chooses the right template according to passed variables.
+     *
+     * @param bool   $hasClaimId
+     * @param string $claimId
+     *
+     * @return array
+     */
+    private static function redirectIfPostRequest(bool $hasClaimId = false, string $claimId = '') {
+
+        if ($hasClaimId && !empty($claimId)) {
+            $returnValue = ['report/dmca', ['claimId' => $claimId]];
+        } else {
+            $returnValue = ['report/dmca'];
+        }
+
+        if (!Request::isPost()) {
+            return $returnValue;
+        }
     }
 }
