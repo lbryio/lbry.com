16085c8aee3d17a51fb7b9a6b61b362b7e18ba2b
eatdostacos <64234158+eatdostacos@users.noreply.github.com>
2020-06-08 21:42:39 -0700

    Edits to Home Page video (#1312)
    * Add spacing between text and video
    
    * Delete unused video page
    
    This is the video page for the video that I just changed to embed style.

--- view/template/download/_videoIntro.php
+++
@@ -1,3 +0,0 @@
-<figure class="video">
-  <video controls poster="https://spee.ch/3/DVFysUg8wSz8ksuVseaI2Z2B.png" src="https://lbryplayer.xyz/content/claims/meetlbrytv/c6c0a5caa6cc391696e1270e33cd9d24ee7c2d52/stream.mp4"></video>
-</figure>
