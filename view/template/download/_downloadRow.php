f12a5533d61e99971f26ab6f1c9f71d43368199e
LaroTayoGaming <larotayogaming@protonmail.com>
2021-01-26 13:36:51 +0800

    added padding on fdroid and android store urls

--- view/template/download/_downloadRow.php
+++ view/template/download/_downloadRow.php
@@ -6,9 +6,9 @@
     <small style="margin-top: 2px; display: inline-block"><a href="/get?showall=1" class="button--link hide--mobile">other platforms</a></small>
   </div>
 
-          <div  style="flex: 1; text-align: center">
+          <div class="odysee" style="flex: 1; text-align: center">
               <a href="https://odysee.com">
-                  <img style="height: 40px;" src="https://odysee.com/public/img/odysee.png" />
+                  <img src="https://odysee.com/public/img/odysee.png" />
               </a>
               <br/>
               <small class="meta" style=" display: inline-block; max-width: 160px">use LBRY in your browser, with <a class="button button--link" href="https://odysee.com">odysee.com</a></small>
