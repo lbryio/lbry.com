1398206819b6eff86008340a0e6a858190128a0f
iamflorencejay <florencejaymunar@protonmail.com>
2021-05-19 14:00:03 +0800

    Added stylesheet changes, featured images, and banner

--- view/template/page/home.php
+++ view/template/page/home.php
@@ -2,27 +2,32 @@
 <?php Response::setMetaDescription(__('description.home')) ?>
 
 <main class="home">
+  <section class="anno__text">
+    <h1 class="anno__title">HELP LBRY SAVE CRYPTO</h1>
+    <div class="anno__subtitle">
+      <div>The SEC doesn’t understand blockchain. The claims made in SEC vs. LBRY would destroy the United States
+        cryptocurrency industry. <br />Learn more at <a id="anno__link" href="https://helplbrysavecrypto.com?ref=lbrycom">helplbrysavecrypto.com</a>
+    </div>
+  </section>
   <section class="home__hero">
     <div class="inner-wrap">
       <aside class="home__cta">
         <div class="home__cta-copy">
-          <h1>LBRY does to publishing,<br/>
-              what Bitcoin did to money.</h1>
-          <p><span class="meta">Join top creators and more than 10,000,000 people on LBRY, an open, free, and fair network for digital content.</span></p>
+          <h1>LBRY does to publishing,<br />
+            what Bitcoin did to money.</h1>
+          <p><span class="meta">Join top creators and more than 10,000,000 people on LBRY, an open, free, and fair
+              network for digital content.</span></p>
           <?php echo View::render('download/_downloadRow') ?>
         </div>
       </aside>
 
       <figure class="home__preview">
         <a href="/get">
-          <video
-            autoplay
-            loop
-            poster="https://spee.ch/f/2019-08-lbry-interface-poster.jpg"
-            playsinline
-          >
-            <source src="https://cdn.lbryplayer.xyz/content/claims/2019-09-lbry-interface-mp4-2/0/stream.mp4" type="video/mp4"/>
-            <source src="https://cdn.lbryplayer.xyz/content/claims/2019-09-lbry-interface-webm-2/b/stream.webm" type="video/webm"/>
+          <video autoplay loop poster="https://spee.ch/f/2019-08-lbry-interface-poster.jpg" playsinline>
+            <source src="https://cdn.lbryplayer.xyz/content/claims/2019-09-lbry-interface-mp4-2/0/stream.mp4"
+              type="video/mp4" />
+            <source src="https://cdn.lbryplayer.xyz/content/claims/2019-09-lbry-interface-webm-2/b/stream.webm"
+              type="video/webm" />
           </video>
         </a>
       </figure>
@@ -31,18 +36,24 @@
 
   <section class="home__callout">
     <h2 style="max-width: 700px">Enjoy the latest content from your favorite creators - as a user, not a product.</h2>
-      <p style="max-width: 800px">This video shows footage from <a href="https://odysee.com">odysee.com</a>, the most popular LBRY app. This same content can be accessed by <a href="/get">LBRY Desktop</a> and other clients as part of the web 3.0 LBRY network.</p>
-      <div class="inner-wrap" style="margin-top: 1rem">
-      <iframe id="lbry-iframe" width="560" height="315" src="https://odysee.com/$/embed/odysee/7a416c44a6888d94fe045241bbac055c726332aa?r=9wKhJPioiNxTBjT6Zoqaf7LNDJcauUjg" allowfullscreen></iframe>
-      </div>
+    <p style="max-width: 800px">This video shows footage from <a href="https://odysee.com">odysee.com</a>, the most
+      popular LBRY app. This same content can be accessed by <a href="/get">LBRY Desktop</a> and other clients as part
+      of the web 3.0 LBRY network.</p>
+    <div class="inner-wrap" style="margin-top: 1rem">
+      <iframe id="lbry-iframe" width="560" height="315"
+        src="https://odysee.com/$/embed/odysee/7a416c44a6888d94fe045241bbac055c726332aa?r=9wKhJPioiNxTBjT6Zoqaf7LNDJcauUjg"
+        allowfullscreen></iframe>
+    </div>
   </section>
 
-    <section class="home__cta">
-        <div class="inner-wrap">
-            <aside class="home__cta"><h2>Use Desktop and Mobile apps for full control.<br/>Use odysee.com for ease.</h2></aside>
-          <?php echo View::render('download/_downloadRow') ?>
-        </div>
-    </section>
+  <section class="home__cta">
+    <div class="inner-wrap">
+      <aside class="home__cta">
+        <h2>Use Desktop and Mobile apps for full control.<br />Use odysee.com for ease.</h2>
+      </aside>
+      <?php echo View::render('download/_downloadRow') ?>
+    </div>
+  </section>
 
   <section class="home__sites">
     <div class="inner-wrap">
@@ -56,6 +67,22 @@
         <h3>LBRY.org</h3>
         <p>Do you have an awesome idea that could use some help? Want to connect with like-minded users?</p>
         <a href="https://lbry.org" class="button button--inverse">Join the party at LBRY.org</a>
+      </aside>
+    </div>
+  </section>
+  <br />
+  <section class="home__asfeatured">
+    <div class="inner-wrap">
+    <h3> As featured on </h3>
+      <aside class="featured__row">
+      <img src="../img/featured/techcrunch.svg"/>
+      <img src="../img/featured/newyorktimes.svg"/>
+      <img src="../img/featured/nationalpublicradio.svg"/>
+      <img src="../img/featured/nasdaq.svg"/>
+      <img src="../img/featured/forbes.svg"/>
+      <img src="../img/featured/reclaimthenet.svg"/>
+      <img src="../img/featured/cointelegraph.svg"/>
+      <img src="../img/featured/bitcoincom.png"/>
       </aside>
     </div>
   </section>
