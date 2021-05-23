16a9fd09814fd840a8950c9d08ef74b3e5458094
Florence Jay Munar <florencejaymunar@protonmail.com>
2021-05-23 15:56:13 +0800

    Update home.php

--- view/template/page/home.php
+++ view/template/page/home.php
@@ -73,7 +73,7 @@
   <br />
   <section class="home__asfeatured">
     <div class="inner-wrap">
-    <h3> As featured on </h3>
+    <h3> As featured on: </h3>
       <aside class="featured__row">
       <img src="../img/featured/techcrunch.svg"/>
       <img src="../img/featured/newyorktimes.svg"/>
