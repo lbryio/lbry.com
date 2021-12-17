ac3cf5ed4aac9ead7b389226dc093805384dd69d
Jeremy Kauffman <jeremy@lbry.com>
2021-12-17 14:46:58 -0500

    remove odysee employees

--- view/template/page/team.php
+++ view/template/page/team.php
@@ -3,7 +3,7 @@
 <?php Response::setMetaDescription('description.team') ?>
 
 <main class="ancillary">
-  <section class="hero" style="background-image: url(https://spee.ch/3cb82a81e95c147686dbf90e9983640939461c53/everyone-banner3.jpg)">
+  <section class="hero" style="background-image: url(https://spee.ch/@lbryteam/everyone-banner2.jpg)">
     <div class="inner-wrap inner-wrap--center-hero">
       <h1>The Team</h1>
       <h2>Teamwork makes the dream work</h2>
@@ -11,12 +11,9 @@
   </section>
 
   <?php $teams = [
-    "Leadership" => ['jeremy-kauffman', 'alex-grintsvayg', 'josh-finer', 'julian-chandra'],
+    "Leadership" => ['jeremy-kauffman', 'alex-grintsvayg', 'josh-finer'],
     "Tech" => ['lex-berezhny',  'jack-robison', 'victor-shyba', 'brannon-king', 'roy-lee',
-               'akinwale-ariwodola', 'jessop-breth',
-               'andrey-beletsky', 'mark-beamer', 'niko-storni',
-               'johnny-nelson'],
-    "Growth" => ['tom-zarebczan', 'eliza-zheng', 'drew-hancock'],
+                'jessop-breth'],
     "Advisory Team" => ['alex-tabarrok', 'ray-carballada', 'stephan-kinsella', 'michael-huemer'],
   ] ?>
 
